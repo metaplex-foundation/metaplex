cb02e68ce5c4e2e6a700ecde760dd50521b28c14
John Rees <john@bitsushi.com>
2021-06-05 12:45:43 +0100

    remove (and .gitignore) .cache directory

--- js/packages/metaplex/.cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-contact-tsx": () => import("./../../../src/pages/contact.tsx" /* webpackChunkName: "component---src-pages-contact-tsx" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
-}
-
