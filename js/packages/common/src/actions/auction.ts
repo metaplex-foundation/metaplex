303f5c4af5ec8a35cc54edaa02884b418f22f063
Jordan Prince <jordanmprince@gmail.com>
2021-06-14 15:32:57 -0500

    Remove excess definitions we dont need from the hack.

--- js/packages/common/src/actions/auction.ts
+++ js/packages/common/src/actions/auction.ts
@@ -310,73 +310,7 @@ export class WinnerLimit {
 // I could think of to intelligently send up u8s without borsh sending up a size param before the array which
 // borsh-rs cant handle
 export class U832ArrayBorshHack {
-  0: number;
-  1: number;
-  2: number;
-  3: number;
-  4: number;
-  5: number;
-  6: number;
-  7: number;
-  8: number;
-  9: number;
-  10: number;
-  11: number;
-  12: number;
-  13: number;
-  14: number;
-  15: number;
-  16: number;
-  17: number;
-  18: number;
-  19: number;
-  20: number;
-  21: number;
-  22: number;
-  23: number;
-  24: number;
-  25: number;
-  26: number;
-  27: number;
-  28: number;
-  29: number;
-  30: number;
-  31: number;
-  constructor(args: {
-    0?: number;
-    1?: number;
-    2?: number;
-    3?: number;
-    4?: number;
-    5?: number;
-    6?: number;
-    7?: number;
-    8?: number;
-    9?: number;
-    10?: number;
-    11?: number;
-    12?: number;
-    13?: number;
-    14?: number;
-    15?: number;
-    16?: number;
-    17?: number;
-    18?: number;
-    19?: number;
-    20?: number;
-    21?: number;
-    22?: number;
-    23?: number;
-    24?: number;
-    25?: number;
-    26?: number;
-    27?: number;
-    28?: number;
-    29?: number;
-    30?: number;
-    31?: number;
-    array?: number[];
-  }) {
+  constructor(args: any) {
     for (let i = 0; i < 32; i++) {
       //@ts-ignore
       if (args.array) this[i] = args.array[i];
