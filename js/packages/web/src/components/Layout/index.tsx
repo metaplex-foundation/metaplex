2187df8b25bc38194326431447d9b5f7cdfc1853
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-06-29 17:15:44 -0700

    fix: set link to head using javascript in hook

--- js/packages/web/src/components/Layout/index.tsx
+++ js/packages/web/src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Layout } from 'antd';
 import { contexts } from '@oyster/common';
 
@@ -12,27 +12,47 @@ const { StorefrontContext } = contexts.Storefront;
 const { Header, Content } = Layout;
 
 const paddingForLayout = (width: number) => {
-  if (width <= 768) return "5px 10px"
-  if (width > 768) return "10px 30px"
-}
+  if (width <= 768) return '5px 10px';
+  if (width > 768) return '10px 30px';
+};
 
 export const AppLayout = React.memo((props: any) => {
   const { width } = useWindowDimensions();
   const { storefront } = useContext(StorefrontContext);
 
+  useEffect(() => {
+    if(!storefront) {
+      return
+    }
+
+    var head = document.head;
+    var link = document.createElement("link");
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = storefront.themeUrl;
+
+    head.appendChild(link);
+
+    return () => { head.removeChild(link); }
+
+  }, [storefront]);
+
   return (
     <>
-      <Layout title={LABELS.APP_TITLE} style={{
-        padding: paddingForLayout(width),
-        maxWidth: 1000,
-      }}>
+      <Layout
+        title={LABELS.APP_TITLE}
+        style={{
+          padding: paddingForLayout(width),
+          maxWidth: 1000,
+        }}
+      >
         <Header className="App-Bar">
           <AppBar />
         </Header>
         <Content style={{ overflow: 'scroll', paddingBottom: 50 }}>
           {props.children}
         </Content>
-        {storefront && <link rel="stylesheet" href={storefront.themeUrl} />}
       </Layout>
     </>
   );
