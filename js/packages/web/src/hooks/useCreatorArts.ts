2334a06973ca281ff7d408f32f5b5e0660011ce5
SULTAN PEYEK <84768757+sultanpeyek@users.noreply.github.com>
2021-07-16 20:51:32 +0700

    Fix unexpected extra and operator (#129)

--- js/packages/web/src/hooks/useCreatorArts.ts
+++ js/packages/web/src/hooks/useCreatorArts.ts
@@ -4,10 +4,11 @@ import { PublicKey } from '@solana/web3.js';
 export const useCreatorArts = (id?: PublicKey | string) => {
   const { metadata } = useMeta();
   const filtered = metadata.filter(
-    m => 
-      m.info.data.creators !== null 
-      && m.info.data.creators?.findIndex(c => c.address.toBase58() === id) !== undefined &&
-      && m.info.data.creators.findIndex(c => c.address.toBase58() === id) >= 0
+    m =>
+      m.info.data.creators !== null &&
+      m.info.data.creators?.findIndex(c => c.address.toBase58() === id) !==
+        undefined &&
+      m.info.data.creators.findIndex(c => c.address.toBase58() === id) >= 0,
   );
 
   return filtered;
