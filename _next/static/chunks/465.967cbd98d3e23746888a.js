ebea379dab34f44e08af57fdd0c45e04d642268c
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2021-07-19 21:37:52 -0500

    Updates

---
+++ _next/static/chunks/465.967cbd98d3e23746888a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[465],{43194:function(){},57017:function(){}}]);
---
+++ _next/static/iedv7pTPNiNGTER6HY41q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-a0adebc5a11788cf0f94.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
