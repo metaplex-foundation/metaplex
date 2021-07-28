47cbb4e6df444880c2404b4bf062ca45f26fbf77
Thomas Lisankie <id@tomlisankie.com>
2021-07-28 13:22:59 -0500

    HowAuctionsWorkModal

---
+++ js/packages/web/src/components/HowAuctionsWorkModal/index.tsx
@@ -0,0 +1,36 @@
+import { InstructionsModal } from '../InstructionsModal';
+import React from 'react';
+import './index.less';
+
+interface HowAuctionsWorkModalProps {
+  buttonClassName: string;
+}
+
+export const HowAuctionsWorkModal: React.FC<HowAuctionsWorkModalProps> = ({
+  buttonClassName,
+}) => {
+  return (
+    <InstructionsModal
+      buttonClassName={buttonClassName}
+      buttonText="How Do Auctions Work?"
+      modalTitle="How Auctions Work"
+      cardProps={[
+        {
+          title: "Place your bid and we'll hold your SOL",
+          description:
+            "SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on Topps' NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.",
+        },
+        {
+          title: 'Win the auction',
+          description:
+            "SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on Topps' NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.",
+        },
+        {
+          title: 'Redeem your NFT',
+          description:
+            "SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on Topps' NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.",
+        },
+      ]}
+    />
+  );
+};
