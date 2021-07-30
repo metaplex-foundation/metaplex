7a01f3c650be9d9e056452a1ab5b4118c020018e
Ilia <56516965+bezrazli4n0@users.noreply.github.com>
2021-07-30 18:33:22 +0300

    feature: success tests for token-metadata (#173)
    * feature: update borsh version, wip testing module
    
    * Fixed signatures for create metadata in tests
    
    * feature: success test for create_metadata_account
    
    * feature: success test for update_metadata_account, added is_mutable param to create method
    
    * feature: success test for update_primary_sale_happened_via_token, added token account creation in test metadata structure
    
    * feature: success test for create_master_edition
    
    * feature: success test for mint_new_edition_from_master_edition_via_token
    
    * feature: added more success tests checks, wip: added necessary accounts
    
    * feature: added more internal mocks, added mint_new_edition_from_master_edition_via_vault_proxy
    
    Co-authored-by: Yuriy Savchenko <yuriy.savchenko@gmail.com>

---
+++ rust/token-metadata/program/tests/update_metadata_account.rs
@@ -0,0 +1,51 @@
+mod utils;
+
+use solana_program_test::*;
+use solana_sdk::signature::Signer;
+use spl_token_metadata::state::Key;
+use utils::*;
+
+#[tokio::test]
+async fn success() {
+    let mut context = program_test().start_with_context().await;
+    let test_metadata = Metadata::new();
+
+    test_metadata
+        .create(
+            &mut context,
+            "Test".to_string(),
+            "TST".to_string(),
+            "uri".to_string(),
+            None,
+            10,
+            true,
+        )
+        .await
+        .unwrap();
+
+    test_metadata
+        .update(
+            &mut context,
+            "Cool".to_string(),
+            "TST".to_string(),
+            "uri".to_string(),
+            None,
+            10,
+        )
+        .await
+        .unwrap();
+
+    let metadata = test_metadata.get_data(&mut context).await;
+
+    assert_eq!(metadata.data.name, "Cool");
+    assert_eq!(metadata.data.symbol, "TST");
+    assert_eq!(metadata.data.uri, "uri");
+    assert_eq!(metadata.data.seller_fee_basis_points, 10);
+    assert_eq!(metadata.data.creators, None);
+
+    assert_eq!(metadata.primary_sale_happened, false);
+    assert_eq!(metadata.is_mutable, true);
+    assert_eq!(metadata.mint, test_metadata.mint.pubkey());
+    assert_eq!(metadata.update_authority, context.payer.pubkey());
+    assert_eq!(metadata.key, Key::MetadataV1);
+}
