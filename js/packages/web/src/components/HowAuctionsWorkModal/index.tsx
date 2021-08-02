07754da3f871780be2612582789cef7c16c2aafa
juan <juandgoc@gmail.com>
2021-08-02 17:48:46 -0500

    how auctions work feedback

--- js/packages/web/src/components/HowAuctionsWorkModal/index.tsx
+++ js/packages/web/src/components/HowAuctionsWorkModal/index.tsx
@@ -16,16 +16,19 @@ export const HowAuctionsWorkModal: React.FC<HowAuctionsWorkModalProps> = ({
       modalTitle="How Auctions Work"
       cardProps={[
         {
-          title: "Place your bid and we'll hold your SOL",
-          description: `SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on ${LABELS.STORE_NAME}'s NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.`,
+          title: 'Placing a Bid',
+          description: `Once you find an NFT you’d like to own, place a bid on the auction page. Keep in mind the amount of SOL you bid will be locked in your wallet for the remainder of the auction.`,
+          imgSrc: '/modals/how-auctions-work-1.jpg',
         },
         {
-          title: 'Win the auction',
-          description: `SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on ${LABELS.STORE_NAME}'s NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.`,
+          title: 'Winning an Auction',
+          description: `Keep an eye on the auctions page (and your notifications) to know when you’ve been outbid, and how the sale is progressing.`,
+          imgSrc: '/modals/how-auctions-work-2.jpg',
         },
         {
-          title: 'Redeem your NFT',
-          description: `SOL is the cryptocurrency used for all transactions on the Solana network, and it’s the currency we use on ${LABELS.STORE_NAME}'s NFTs. All of the NFTs on our platform can be purchased with SOL. Creators get paid in it, too.`,
+          title: 'Redeeming your NFT',
+          description: `If you’re lucky enough to win your NFT auction, you’ll have to redeem it to add it to your wallet. This can be done from the auction, winning notification, or your profile on ${LABELS.STORE_NAME}.`,
+          imgSrc: '/modals/how-auctions-work-3.jpg',
         },
       ]}
     />
