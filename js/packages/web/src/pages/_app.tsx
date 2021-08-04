7e224c7b12f041b8fb9867f7f83bb06cf0565d49
exromany <exromany@gmail.com>
2021-08-04 19:59:15 +0300

    chore: organize styles

--- js/packages/web/src/pages/_app.tsx
+++ js/packages/web/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import '../styles.less';
+import '../styles/index.less';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
