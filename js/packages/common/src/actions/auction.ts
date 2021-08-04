8fac1aca138dbce4137e9921726e3ccb79408265
Jordan Prince <jordanmprince@gmail.com>
2021-08-04 12:55:24 -0500

    Analytics (#185)
    * Built out logic for analytics view
    
    * Scope to sol only
    
    * WIP commit on adding visuals and fixing rendering
    
    * Analytics 1.0 done. Please delete later.
    
    * Dont spam rpc if on all mode

--- js/packages/common/src/actions/auction.ts
+++ js/packages/common/src/actions/auction.ts
@@ -52,6 +52,16 @@ export class BidState {
     }
   }
 
+  public getAmountAt(winnerIndex: number): BN | null {
+    const convertedIndex = this.bids.length - winnerIndex - 1;
+
+    if (convertedIndex >= 0 && convertedIndex < this.bids.length) {
+      return this.bids[convertedIndex].amount;
+    } else {
+      return null;
+    }
+  }
+
   public getWinnerIndex(bidder: PublicKey): number | null {
     if (!this.bids) return null;
 
@@ -215,8 +225,6 @@ export class AuctionData {
   state: AuctionState;
   /// Auction Bids, each user may have one bid open at a time.
   bidState: BidState;
-  /// Total uncancelled bids
-  totalUncancelledBids: BN;
   /// Used for precalculation on the front end, not a backend key
   bidRedemptionKey?: PublicKey;
 
@@ -285,7 +293,6 @@ export class AuctionData {
     this.priceFloor = args.priceFloor;
     this.state = args.state;
     this.bidState = args.bidState;
-    this.totalUncancelledBids = args.totalUncancelledBids;
   }
 }
 
