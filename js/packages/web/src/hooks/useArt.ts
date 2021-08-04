cd5cb2ed05434454dbd6c02c231a6fb0a3ee415b
exromany <exromany@gmail.com>
2021-08-04 20:17:59 +0300

    small hook improvements

--- js/packages/web/src/hooks/useArt.ts
+++ js/packages/web/src/hooks/useArt.ts
@@ -14,6 +14,7 @@ import {
 import { WhitelistedCreator } from '../models/metaplex';
 import { Cache } from 'three';
 import { useInView } from 'react-intersection-observer';
+import { pubkeyToString } from '../utils/pubkeyToString';
 
 const metadataToArt = (
   info: Metadata | undefined,
@@ -137,7 +138,7 @@ export const useArt = (id?: PublicKey | string) => {
   const { metadata, editions, masterEditions, whitelistedCreatorsByCreator } =
     useMeta();
 
-  const key = typeof id === 'string' ? id : id?.toBase58() || '';
+  const key = pubkeyToString(id);
 
   const account = useMemo(
     () => metadata.find(a => a.pubkey.toBase58() === key),
@@ -165,7 +166,7 @@ export const useExtendedArt = (id?: PublicKey | string) => {
   const { ref, inView } = useInView();
   const localStorage = useLocalStorage();
 
-  const key = typeof id === 'string' ? id : id?.toBase58() || '';
+  const key = pubkeyToString(id);
 
   const account = useMemo(
     () => metadata.find(a => a.pubkey.toBase58() === key),
