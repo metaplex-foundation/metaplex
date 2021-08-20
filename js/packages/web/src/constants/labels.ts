075d69bd002bcc7902a1818e6c8a97df2e35a682
yasuoyuhao <yasuoyuhao@klearthink.com>
2021-08-20 14:15:32 +0800

    fix(labels): fix labels

--- js/packages/web/src/constants/labels.ts
+++ js/packages/web/src/constants/labels.ts
@@ -6,7 +6,7 @@ export const LABELS = {
   ACCOUNT_FUNDED: 'Account funded.',
   MENU_HOME: 'Home',
   MENU_FAUCET: 'Faucet',
-  APP_TITLE: 'Metaplex',
+  APP_TITLE: 'Circlepod',
   CONNECT_BUTTON: 'Connect',
   WALLET_TOOLTIP: 'Wallet public key',
   WALLET_BALANCE: 'Wallet balance',
