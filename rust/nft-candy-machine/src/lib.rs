d6824cde7fca2fc9bf05ff3cc83dad34ddfc08af
Jordan Prince <jordanmprince@gmail.com>
2021-08-22 18:14:15 -0500

    Commit where is top using anchor

--- rust/nft-candy-machine/src/lib.rs
+++ rust/nft-candy-machine/src/lib.rs
@@ -3,7 +3,7 @@ pub mod utils;
 use {
     crate::utils::{assert_initialized, assert_owned_by},
     anchor_lang::{prelude::*, solana_program::system_program},
-    spl_token::state::Mint,
+    spl_token::state::{Account, Mint},
 };
 
 const PREFIX: &str = "candy_machine";
@@ -11,25 +11,38 @@ const PREFIX: &str = "candy_machine";
 pub mod nft_candy_machine {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>, price: u64, items_available: u64) -> ProgramResult {
-        let candy_machine = &mut ctx.accounts.candy_machine;
+        /*   let candy_machine = &mut ctx.accounts.candy_machine;
         candy_machine.price = price;
         candy_machine.items_available = items_available;
+        candy_machine.wallet = *ctx.accounts.wallet.key;
+        candy_machine.authority = *ctx.accounts.authority.key;
+        candy_machine.config = *ctx.accounts.config.key;
         if ctx.remaining_accounts.len() > 0 {
             let token_mint_info = &ctx.remaining_accounts[0];
             let _token_mint: Mint = assert_initialized(&token_mint_info)?;
+            let token_account: Account = assert_initialized(&ctx.accounts.wallet)?;
+
             assert_owned_by(&token_mint_info, &spl_token::id())?;
+            assert_owned_by(&ctx.accounts.wallet, &spl_token::id())?;
+
+            if token_account.mint != *token_mint_info.key {
+                return Err(ErrorCode::MintMismatch.into());
+            }
+
             candy_machine.token_mint = Some(*token_mint_info.key);
-        }
+        }*/
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, seeds=[PREFIX.as_bytes(), config.key.as_ref()], has_one = authority, has_one = wallet, has_one = config, payer=payer)]
+    #[account(init, seeds=[PREFIX.as_bytes(), config.key.as_ref()], payer=payer, space=8+32+32+33+32+64+64+64+200)]
     candy_machine: ProgramAccount<'info, CandyMachine>,
+    #[account(constraint= !wallet.data_is_empty() || wallet.lamports() > 0 )]
     wallet: AccountInfo<'info>,
     config: AccountInfo<'info>,
+    #[account(constraint= !authority.data_is_empty() || authority.lamports() > 0 )]
     authority: AccountInfo<'info>,
     #[account(mut, signer)]
     payer: AccountInfo<'info>,
@@ -56,4 +69,6 @@ pub enum ErrorCode {
     IncorrectOwner,
     #[msg("Account is not initialized!")]
     Uninitialized,
+    #[msg("Mint Mismatch!")]
+    MintMismatch,
 }
