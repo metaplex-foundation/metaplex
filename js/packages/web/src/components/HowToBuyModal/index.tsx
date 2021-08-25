5f8df5db1c77d92f0ce77857a9d54f2adb9a2636
juan <juandgoc@gmail.com>
2021-08-25 14:24:43 -0500

    added metaplex logo to modal

--- js/packages/web/src/components/HowToBuyModal/index.tsx
+++ js/packages/web/src/components/HowToBuyModal/index.tsx
@@ -30,7 +30,7 @@ export const HowToBuyModal: React.FC<HowToBuyModalProps> = ({
         },
         {
           title: `Connect your wallet to ${LABELS.STORE_NAME}.`,
-          imgSrc: '/modals/how-to-buy-3.svg',
+          imgSrc: '/modals/how-to-buy-3.jpg',
           description: `To connect your wallet, tap “Connect Wallet” here on the site. Select the Phantom option, and your wallet will connect. After that, you can start bidding on NFTs.`,
           endElement: <ConnectButton className={'secondary-btn'} />,
         },
