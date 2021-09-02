265a9aedd5514ea0ba3c4fadf517c508ba54d947
DecentSol <decentsol@protonmail.com>
2021-09-02 06:41:41 -0700

    Updates

---
+++ _next/static/1X0n0AzmLN4X_p60FtONi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-77bea5f09d68a4e44ee1.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
