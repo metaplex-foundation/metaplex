40cfa295117d50dbf2593b81b9c63f0fcf22a17b
Jordan Prince <jordanmprince@gmail.com>
2021-09-04 15:32:45 -0500

    A few candy machine fixes - to uploader and program (#304)

--- rust/nft-candy-machine/src/lib.rs
+++ rust/nft-candy-machine/src/lib.rs
@@ -351,6 +351,7 @@ pub mod nft_candy_machine {
                 position_from_right
             );
             if old_value_in_vec != data[my_position_in_vec] {
+                msg!("Increasing count");
                 new_count = new_count
                     .checked_add(1)
                     .ok_or(ErrorCode::NumericalOverflowError)?;
@@ -395,7 +396,7 @@ pub mod nft_candy_machine {
         }
 
         if get_config_count(&ctx.accounts.config.to_account_info().data.borrow())?
-            != candy_machine.data.items_available as usize
+            < candy_machine.data.items_available as usize
         {
             return Err(ErrorCode::ConfigLineMismatch.into());
         }
@@ -610,6 +611,6 @@ pub enum ErrorCode {
     CandyMachineEmpty,
     #[msg("Candy machine is not live yet!")]
     CandyMachineNotLiveYet,
-    #[msg("Number of config lines must match items available")]
+    #[msg("Number of config lines must be at least number of items available")]
     ConfigLineMismatch,
 }
