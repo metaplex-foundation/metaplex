43f38f2d08e736374d3bf4cf18e4ba87c0776e64
Martin Garcia <martingarcia@Martins-MacBook-Pro.local>
2021-09-06 17:03:33 -0700

    Updates

--- _next/static/xpa2Xz1R6RJJwuwakj7Ki/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-176b7b602ad1927aaae4.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
