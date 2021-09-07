6a374ebccd6ed6be34f90387ded9a2fd49eae9dc
daniil <daniil.agarev@convexitydmcc.com>
2021-09-07 11:23:16 +0400

    Add registration form to Marketplace

---
+++ js/packages/web/src/views/registration/index.tsx
@@ -0,0 +1,20 @@
+import {SignUpForm} from "../../components/SignUpForm";
+import {Col, Divider, Row} from "antd";
+
+
+export const RegistrationView = () => {
+  return (
+    <>
+      <Row>
+        <Col span={24}>
+          <Divider>Registration</Divider>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} style={{margin: 0}}>
+          <SignUpForm/>
+        </Col>
+      </Row>
+    </>
+  )
+};
