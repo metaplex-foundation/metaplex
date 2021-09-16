00fa2f532f473b41baa15541ff6bbf2b0900181b
daniil <daniil.agarev@convexitydmcc.com>
2021-09-16 12:55:06 +0400

    Add getting attributes from backend, for auction

--- js/packages/web/src/views/auction/index.tsx
+++ js/packages/web/src/views/auction/index.tsx
@@ -34,6 +34,7 @@ import useWindowDimensions from '../../utils/layout';
 import { CheckOutlined } from '@ant-design/icons';
 import { useMemo } from 'react';
 import { ArtType } from '../../types';
+import {getAttributesByNftId} from "../../actions/lmsIntegration";
 import { ClickToCopy  } from '../../components/ClickToCopy';
 
 export const AuctionItem = ({
@@ -80,6 +81,7 @@ export const AuctionView = () => {
   const { env } = useConnectionConfig();
   const auction = useAuction(id);
   const [currentIndex, setCurrentIndex] = useState(0);
+  let [attributes, setAttributes] = useState({});
   const art = useArt(auction?.thumbnail.metadata.pubkey);
   const { ref, data } = useExtendedArt(auction?.thumbnail.metadata.pubkey);
   const creators = useCreators(auction);
@@ -96,8 +98,17 @@ export const AuctionView = () => {
 
   const hasDescription = data === undefined || data.description === undefined;
   const description = data?.description;
-  const attributes = data?.attributes;
-
+  // const attributes = data?.attributes;
+  useEffect(() => {
+    if (data !== undefined) {
+      getAttributesByNftId(id).then(res => {
+        setAttributes(res);
+      }).catch(e => {
+        console.log(e);
+      })
+    }
+    return;
+  }, [data])
   const items = [
     ...(auction?.items
       .flat()
@@ -166,9 +177,9 @@ export const AuctionView = () => {
             <>
               <h6>Attributes</h6>
               <List grid={{ column: 4 }}>
-                {attributes.map(attribute => (
-                  <List.Item>
-                    <Card title={attribute.trait_type}>{attribute.value}</Card>
+                {Object.entries(attributes).map((item: [string, any], idx) =>  (
+                  <List.Item key={idx}>
+                    <Card title={item[0]}>{item[1]}</Card>
                   </List.Item>
                 ))}
               </List>
@@ -332,7 +343,7 @@ const BidLine = (props: {
             address={bidder}
           />{' '}
           {bidderTwitterHandle ? (
-            <Row className="pubkey-row"> 
+            <Row className="pubkey-row">
               <a
                 target="_blank"
                 title={shortenAddress(bidder)}
@@ -341,7 +352,7 @@ const BidLine = (props: {
               <ClickToCopy className="copy-pubkey" copyText={bidder as string} />
             </Row>
           ) : (
-            <Row className="pubkey-row"> 
+            <Row className="pubkey-row">
               {shortenAddress(bidder)}
               <ClickToCopy className="copy-pubkey" copyText={bidder as string} />
             </Row>
