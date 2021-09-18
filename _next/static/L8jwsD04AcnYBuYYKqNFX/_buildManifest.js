8d0725116066872f27f8eb12c254f3fc82a0c4b8
Dan Charbonneau <daniel@charbo.net>
2021-09-17 18:14:24 -0700

    Updates

---
+++ _next/static/L8jwsD04AcnYBuYYKqNFX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-aa1913dee32491ce8e04.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
