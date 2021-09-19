3d1117241e4398a02281f6d89a2b92bbe56a8463
Slavomir <gagliardetto@users.noreply.github.com>
2021-09-19 15:18:31 +0200

    Improve comment

--- rust/auction/program/src/instruction.rs
+++ rust/auction/program/src/instruction.rs
@@ -56,7 +56,7 @@ pub enum AuctionInstruction {
     StartAuction(StartAuctionArgs),
 
     /// Update the authority for an auction account.
-    ///   0. `[writable]` auction
+    ///   0. `[writable]` auction (pda of ['auction', program id, resource id])
     ///   1. `[signer]` authority
     ///   2. `[]` newAuthority
     SetAuthority,
