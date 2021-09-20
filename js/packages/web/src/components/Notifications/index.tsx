3e49480b14f3c86bfdbdb306589addb1e6c5457d
Jordan Prince <jordanmprince@gmail.com>
2021-09-20 15:26:11 -0500

    Fix more differences

--- js/packages/web/src/components/Notifications/index.tsx
+++ js/packages/web/src/components/Notifications/index.tsx
@@ -487,7 +487,7 @@ export function Notifications() {
       content={content}
       trigger="click"
     >
-      <h1 className="title">J</h1>
+      <h1 className="title">M</h1>
     </Popover>
   );
 
