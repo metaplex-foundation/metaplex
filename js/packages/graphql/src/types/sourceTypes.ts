2eff171253b6dd099970724b702dafedf73ee918
exromany <exromany@gmail.com>
2021-09-20 22:40:30 +0300

    wrapPubkey for Map

--- js/packages/graphql/src/types/sourceTypes.ts
+++ js/packages/graphql/src/types/sourceTypes.ts
@@ -7,11 +7,11 @@ import type {
 } from "../common";
 
 export type FieldPubkey = { pubkey: string };
+export type Fields<T> = T & FieldPubkey;
 
-export type Artwork = Meta & FieldPubkey;
-export type Creator = WhitelistedCreator & FieldPubkey;
+export type Artwork = Fields<Meta>;
+export type Creator = Fields<WhitelistedCreator>;
 
-export type Auction = AuctionData &
-  FieldPubkey & {
-    manager: (AuctionManagerV1 | AuctionManagerV2) & FieldPubkey;
-  };
+export type Auction = Fields<AuctionData> & {
+  manager: (AuctionManagerV1 | AuctionManagerV2) & FieldPubkey;
+};
