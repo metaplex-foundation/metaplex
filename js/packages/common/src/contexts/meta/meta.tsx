3532d5f11ef6da15b4af6b1c2244411e3269a911
Jordan Prince <jordanmprince@gmail.com>
2021-09-20 15:26:37 -0500

    Speed Run Mode (#448)
    * Fix
    
    * stuff
    
    * Fix
    
    * stuff
    
    * Change store ids
    
    * Change name
    
    * No warp speed limit
    
    * Metaplex is really f-ing fast.
    
    * Make it optional
    
    * Fixes for original mode
    
    * remove specific identifiers
    
    * Fix more differences

--- js/packages/common/src/contexts/meta/meta.tsx
+++ js/packages/common/src/contexts/meta/meta.tsx
@@ -2,7 +2,11 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { queryExtendedMetadata } from './queryExtendedMetadata';
 import { subscribeAccountsChange } from './subscribeAccountsChange';
 import { getEmptyMetaState } from './getEmptyMetaState';
-import { loadAccounts } from './loadAccounts';
+import {
+  limitedLoadAccounts,
+  loadAccounts,
+  USE_SPEED_RUN,
+} from './loadAccounts';
 import { MetaContextState, MetaState } from './types';
 import { useConnection } from '../connection';
 import { useStore } from '../store';
@@ -57,7 +61,9 @@ export function MetaProvider({ children = null as any }) {
 
       console.log('-----> Query started');
 
-      const nextState = await loadAccounts(connection, all);
+      const nextState = !USE_SPEED_RUN
+        ? await loadAccounts(connection, all)
+        : await limitedLoadAccounts(connection);
 
       console.log('------->Query finished');
 
