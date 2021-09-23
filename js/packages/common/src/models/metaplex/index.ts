6c8ef21ce2753eb4e5538e2cfd83097c4a883b8a
Kevin Guevara <kevin@monadical.com>
2021-09-23 10:30:19 -0500

    artists page

--- js/packages/common/src/models/metaplex/index.ts
+++ js/packages/common/src/models/metaplex/index.ts
@@ -502,6 +502,7 @@ export class WhitelistedCreator {
   name?: string;
   image?: string;
   description?: string;
+  background?: string;
 
   constructor(args: { address: string; activated: boolean }) {
     this.address = args.address;
