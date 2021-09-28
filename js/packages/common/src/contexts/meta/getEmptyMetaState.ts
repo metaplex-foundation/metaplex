d181ae8f541d99a98597b5c9ed3fc704a8577a11
Jordan Prince <jordanmprince@gmail.com>
2021-09-28 14:10:32 -0500

    Adding states to front end and indexer action

--- js/packages/common/src/contexts/meta/getEmptyMetaState.ts
+++ js/packages/common/src/contexts/meta/getEmptyMetaState.ts
@@ -22,4 +22,6 @@ export const getEmptyMetaState = (): MetaState => ({
   prizeTrackingTickets: {},
   safetyDepositConfigsByAuctionManagerAndIndex: {},
   bidRedemptionV2sByAuctionManagerAndWinningIndex: {},
+  auctionCaches: {},
+  storeIndexer: [],
 });
