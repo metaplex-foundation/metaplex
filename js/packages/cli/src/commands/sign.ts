55977a6fbed3923bc9d8028bbe88b45006e72fd0
ghostoutfit <josephlkremer@gmail.com>
2021-09-30 11:14:43 -0400

    Update sign.ts

--- js/packages/cli/src/commands/sign.ts
+++ js/packages/cli/src/commands/sign.ts
@@ -1,8 +1,10 @@
 import { Keypair, PublicKey, TransactionInstruction } from '@solana/web3.js';
 import { TOKEN_METADATA_PROGRAM_ID } from '../helpers/constants';
 import { sendTransactionWithRetryWithKeypair } from '../helpers/transactions';
-import { loadAnchorProgram, loadWalletKey } from '../helpers/accounts';
+import { loadCandyProgram, loadWalletKey } from '../helpers/accounts';
 import { Program } from '@project-serum/anchor';
+
+const METADATA_SIGNATURE = Buffer.from([7]); //now thats some voodoo magic. WTF metaplex? XD
 
 export async function signMetadata(
   metadata: string,
@@ -10,38 +12,14 @@ export async function signMetadata(
   env: string,
 ) {
   const creatorKeyPair = loadWalletKey(keypair);
-  const anchorProgram = await loadAnchorProgram(creatorKeyPair, env);
-  await signWithRetry(anchorProgram, creatorKeyPair, metadata);
-}
-
-export async function signAllUnapprovedMetadata(keypair: string, env: string) {
-  const creatorKeyPair = loadWalletKey(keypair);
-  const anchorProgram = await loadAnchorProgram(creatorKeyPair, env);
-  const metadataIds = await findAllUnapprovedMetadataIds(
-    anchorProgram,
-    creatorKeyPair,
-  );
-
-  for (const id in metadataIds) {
-    await signWithRetry(anchorProgram, creatorKeyPair, id);
-  }
-}
-
-/* eslint-disable */
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function findAllUnapprovedMetadataIds(
-  anchorProgram: Program,
-  creatorKeyPair: Keypair,
-): Promise<string[]> {
-  //TODO well I need some help with that... so... help? :D
-  throw new Error('Unsupported yet');
+  const anchorProgram = await loadCandyProgram(creatorKeyPair, env);
+  await signWithRetry(anchorProgram, creatorKeyPair, new PublicKey(metadata));
 }
 
 async function signWithRetry(
   anchorProgram: Program,
   creatorKeyPair: Keypair,
-  metadataAddress: string,
+  metadataAddress: PublicKey,
 ) {
   await sendTransactionWithRetryWithKeypair(
     anchorProgram.provider.connection,
@@ -61,7 +39,7 @@ export function signMetadataInstruction(
   metadata: PublicKey,
   creator: PublicKey,
 ): TransactionInstruction {
-  const data = Buffer.from([7]); //now thats bloody magic. WTF metaplex? XD
+  const data = METADATA_SIGNATURE;
 
   const keys = [
     {
