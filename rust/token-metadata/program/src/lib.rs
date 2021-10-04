17326504cc307d947ef035b8f01c347ebbc216a5
Jordan Prince <jordanmprince@gmail.com>
2021-10-04 08:49:33 -0500

    Remove auction cli nobody uses

--- rust/token-metadata/program/src/lib.rs
+++ rust/token-metadata/program/src/lib.rs
@@ -11,4 +11,4 @@ pub mod utils;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+solana_program::declare_id!("GCUQ7oWCzgtRKnHnuJGxpr5XVeEkxYUXwTKYcqGtxLv4");
