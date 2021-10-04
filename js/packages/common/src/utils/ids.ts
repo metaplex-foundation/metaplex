8abf1327441bf731a9095deb51fe6a06796f96ca
juan <juandgoc@gmail.com>
2021-10-04 17:40:41 -0500

    fixed programs ids

--- js/packages/common/src/utils/ids.ts
+++ js/packages/common/src/utils/ids.ts
@@ -65,24 +65,16 @@ export const MEMO_ID = new PublicKey(
   'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr',
 );
 
-//export const METADATA_PROGRAM_ID =
-// 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s' as StringPublicKey;
-
 export const METADATA_PROGRAM_ID =
-  'GCUQ7oWCzgtRKnHnuJGxpr5XVeEkxYUXwTKYcqGtxLv4' as StringPublicKey;
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s' as StringPublicKey;
 
-//export const VAULT_ID =
-// 'vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn' as StringPublicKey;
 export const VAULT_ID =
-  '41cCnZ1Z1upJdtsS1tzFGR34cPFgJLzvJFmgYKpCqkz7' as StringPublicKey;
-//export const AUCTION_ID =
-//  'auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8' as StringPublicKey;
-export const AUCTION_ID =
-  '6u5XVthCStUfmNrYhFsST94oKxzwEZfZFHFhiCnB2nR1' as StringPublicKey;
+  'vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn' as StringPublicKey;
 
-//export const METAPLEX_ID =
-//  'p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98' as StringPublicKey;
+export const AUCTION_ID =
+  'auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8' as StringPublicKey;
+
 export const METAPLEX_ID =
-  '98jcGaKLKx9vv33H9edLUXAydrSipHhJGDQuPXBVPVGp' as StringPublicKey;
+  'p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98' as StringPublicKey;
 
 export const SYSTEM = new PublicKey('11111111111111111111111111111111');
