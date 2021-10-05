a39ee777e4a59ebd7c652e53d60d8c379ca8c808
Daniel Miller <danieladammiller@gmail.com>
2021-10-04 21:57:03 -0700

    overwrite source maps to prevent build errors

--- js/sourcemap.sh
+++ js/sourcemap.sh
@@ -1,1 +1,1 @@
-npx bugsnag-source-maps upload-node --api-key $BUGSNAG_API_KEY  --directory=packages/web/.next/static/chunks/
+npx bugsnag-source-maps upload-node --api-key $BUGSNAG_API_KEY  --directory=packages/web/.next/static/chunks/ --overwrite
