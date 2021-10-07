95793cc3835512715961e27cb227ec023ec01a7e
exromany <exromany@gmail.com>
2021-10-07 10:55:25 +0300

    chore: drop unused utils & bin

--- js/packages/graphql/src/utils/createOrm.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export async function createOrm(mongoConnString: string) {
-  const client = new MongoClient(mongoConnString);
-  await client.connect();
-  return client.db("metaplex");
-}
