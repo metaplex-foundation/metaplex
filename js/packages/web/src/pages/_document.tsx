1a625bf2c37618253040bd7f599a3b806f38f7cb
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2021-10-12 13:26:48 -0700

    update favicon

--- js/packages/web/src/pages/_document.tsx
+++ js/packages/web/src/pages/_document.tsx
@@ -24,20 +24,8 @@ export default class MetaplexDocument extends Document {
           <link
             rel="icon"
             type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon.png"
           />
           <meta name="theme-color" content="#000000" />
           <meta name="description" content="Metaplex NFT Marketplace" />
