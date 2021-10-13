eeaa4bfd377148f5b5a2c18600e3e78dab26c9e0
checho1989 <cheverdolaga@hotmail.com>
2021-10-13 13:41:22 -0400

    Fix merge

--- js/packages/web/src/pages/_app.tsx
+++ js/packages/web/src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Metaplex NFT Marketplace</title>
+        <title>Synqtec NFT Marketplace</title>
       </Head>
       <div id="root">
         <Component {...pageProps} />
--- js/packages/web/src/pages/_document.tsx
+++ js/packages/web/src/pages/_document.tsx
@@ -40,7 +40,7 @@ export default class MetaplexDocument extends Document {
             href="/favicon-16x16.png"
           />
           <meta name="theme-color" content="#000000" />
-          <meta name="description" content="Metaplex NFT Marketplace" />
+          <meta name="description" content="Synqtec NFT Marketplace" />
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="stylesheet"
