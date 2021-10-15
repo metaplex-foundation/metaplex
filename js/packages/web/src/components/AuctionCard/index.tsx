2bae41d6baf9eb32db3db96465450018841fd48c
Daniel <daniel.f.lopez.j@gmail.com>
2021-10-15 10:25:44 -0500

    fix place a bid message

--- js/packages/web/src/components/AuctionCard/index.tsx
+++ js/packages/web/src/components/AuctionCard/index.tsx
@@ -250,7 +250,7 @@ export const AuctionCard = ({
   const gapTick = auctionExtended
     ? auctionExtended.info.gapTickSizePercentage
     : 0;
-  const tickSize = auctionExtended ? auctionExtended.info.tickSize : 0;
+  const tickSize = auctionExtended?.info?.tickSize ? auctionExtended.info.tickSize : 0;
   const tickSizeInvalid = !!(
     tickSize &&
     value &&
@@ -597,7 +597,7 @@ export const AuctionCard = ({
                         ? `◎ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
                         : ''
                     }
-                    placeholder={`Bid ${minBid} SOL or more`}
+                    placeholder={ minBid === 0 ? `Place a Bid` : `Bid ${minBid} SOL or more` }
                   />
                 </div>
                 <div className={'bid-buttons'}>
