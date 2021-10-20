d5591e97cec4f67fb6c97d0c0c36a3f31763f395
checho1989 <cheverdolaga@hotmail.com>
2021-10-20 00:07:26 -0400

    Fix mainnet commits

--- js/packages/common/src/contexts/connection.tsx
+++ js/packages/common/src/contexts/connection.tsx
@@ -42,7 +42,7 @@ export type ENV =
 export const ENDPOINTS = [
   {
     name: 'mainnet-beta' as ENV,
-    endpoint: 'http://3.22.104.109:8900',
+    endpoint: 'https://api.metaplex.solana.com/',
     ChainId: ChainId.MainnetBeta,
   },
   {
