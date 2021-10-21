00cac4388f55648809d38cb5f199a52b047d459e
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2021-10-21 09:57:23 -0700

    new arweave wallet

--- js/packages/web/src/utils/ids.ts
+++ js/packages/web/src/utils/ids.ts
@@ -2,5 +2,5 @@ import { PublicKey } from '@solana/web3.js';
 
 // TODO: generate key ---
 export const AR_SOL_HOLDER_ID = new PublicKey(
-  'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm',
+  '6FKvsq4ydWFci6nGq9ckbjYMtnmaqAoatz5c9XWjiDuS',
 );
