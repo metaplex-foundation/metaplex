0ae179bbe33c80f63d958d35971d9b4833157cbd
zen0m <zen0mNFT@protonmail.com>
2021-10-23 10:37:10 -0700

    move logo location + update path

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -109,7 +109,7 @@ const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'js/assets/wallets/mf-logo.svg'} />
+      <img src={'/mf-logo.svg'} />
     </Link>
   );
 };
