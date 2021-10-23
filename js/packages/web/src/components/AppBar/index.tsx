253ee15bcb5f88950e01d627c7da342e0ce252ec
zen0m <zen0mNFT@protonmail.com>
2021-10-23 09:00:37 -0700

    uploading logo and editing path

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -109,7 +109,7 @@ const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/metaplex-logo.svg'} />
+      <img src={'/mf-logo.svg'} />
     </Link>
   );
 };
