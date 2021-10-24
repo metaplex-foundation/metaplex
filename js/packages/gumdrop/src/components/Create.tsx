63cd8f361b76a4eeb4c80f7c0b282d14d60dbfb1
Lawrence Wu <lawrencejwu@gmail.com>
2021-10-24 17:16:26 -0500

    Add short-circuit re-send if distribution last has claim URLs
    - This just effectively wraps a loop around the distribution

--- js/packages/gumdrop/src/components/Create.tsx
+++ js/packages/gumdrop/src/components/Create.tsx
@@ -94,6 +94,34 @@ const setupMailchimp = (auth : string, source : string) => {
   };
 }
 
+const setupManual = (auth : string, source : string) => {
+  return async (
+    info : ClaimantInfo,
+    mintUrl: string,
+  ) => {
+    // TODO duplicated work since claim URLs are available for download
+    // regardless...
+    console.log({
+      "handle": info.handle,
+      "url": info.url,
+    });
+  };
+}
+
+const setupSender = (
+  method : string,
+  auth : string,
+  source : string,
+) => {
+  if (method === "Mailchimp") {
+    return setupMailchimp(auth, source);
+  } else if (method === "Manual") {
+    return setupManual(auth, source);
+  } else {
+    throw new Error(`Unrecognized claim distribution method ${method}`);
+  }
+}
+
 const parseClaimants = (
   input : string
 ) : Array<ClaimantInfo> => {
@@ -102,6 +130,10 @@ const parseClaimants = (
     return {
       handle : obj.handle,
       amount : obj.amount,
+      pin    : obj.pin,
+      bump   : obj.bump,
+      pda    : obj.pda,
+      url    : obj.url,
     };
   });
 };
@@ -119,23 +151,13 @@ export const Create = (
   const [commSource, setCommSource] = React.useState("");
   const [filename, setFilename] = React.useState("");
   const [text, setText] = React.useState("");
-  const [claimURLs, setClaimURLs] = React.useState<Array<string>>([]);
+  const [claimURLs, setClaimURLs] = React.useState<Array<ClaimantInfo>>([]);
 
   const submit = async (e : React.SyntheticEvent) => {
     e.preventDefault();
 
     if (!wallet.connected || wallet.publicKey === null) {
       throw new Error(`Wallet not connected`);
-    }
-
-    const claimants = parseClaimants(text);
-    const totalClaim = claimants.reduce((acc, c) => acc + c.amount, 0);
-    claimants.forEach(c => {
-      c.pin = randomBytes();
-    });
-
-    if (claimants.length === 0) {
-      throw new Error(`No claimants provided`);
     }
 
     let mintKey : PublicKey;
@@ -156,6 +178,39 @@ export const Create = (
     }
     // const mintInfo = MintLayout.decode(Buffer.from(mintAccount.data));
 
+    const mintUrl = `https://explorer.solana.com/address/${mintKey.toBase58()}?cluster=${Connection.envFor(connection)}`;
+    console.log(mintUrl);
+
+
+    const claimants = parseClaimants(text);
+    if (claimants.length === 0) {
+      throw new Error(`No claimants provided`);
+    }
+
+    const mightHaveExisting = (info : ClaimantInfo) => {
+      // TODO: others?
+      return info.url !== undefined && info.url !== null;
+    };
+    if (claimants.reduce((acc, c) => acc && mightHaveExisting(c), true)) {
+      // TODO: more validation of URLs? The creator is using they're own
+      // credentials to re-send so if they're malicious it's not that bad
+      // right?...
+      const resendOnly = window.confirm("Uploaded distribution list has URLs "
+          + "for all claimants. Skip creation of airdrop and only re-send links?");
+      if (resendOnly) {
+        const sender = setupSender(commMethod, commAuth, commSource);
+        for (let idx = 0; idx < claimants.length; ++idx) {
+          await sender(claimants[idx], mintUrl);
+        }
+        return;
+      } else {
+        // Bail? another confirm to continue with creation?
+      }
+    }
+    const totalClaim = claimants.reduce((acc, c) => acc + c.amount, 0);
+    claimants.forEach(c => {
+      c.pin = randomBytes();
+    });
 
     const [creatorTokenKey, ] = await PublicKey.findProgramAddress(
       [
@@ -220,28 +275,6 @@ export const Create = (
       SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
     );
 
-    const mintUrl = `https://explorer.solana.com/address/${mintKey.toBase58()}?cluster=${Connection.envFor(connection)}`;
-
-    let sender;
-    if (commMethod === "Mailchimp") {
-      sender = setupMailchimp(commAuth, commSource);
-    } else if (commMethod === "Manual") {
-      console.log(mintUrl);
-      sender = async (
-          info : ClaimantInfo,
-          mintUrl: string,
-        ) => {
-          // TODO duplicated work since claim URLs are available for download
-          // regardless...
-          console.log({
-            "handle": info.handle,
-            "claim": info.url,
-          });
-        };
-    } else {
-      throw new Error(`Unrecognized claim distribution method ${commMethod}`);
-    }
-
     for (let idx = 0; idx < claimants.length; ++idx) {
       const proof = tree.getProof(idx);
       const verified = tree.verifyProof(idx, proof, root);
@@ -265,7 +298,7 @@ export const Create = (
     }
 
     // TODO: defer until success?
-    setClaimURLs(claimants.map(c => c.url));
+    setClaimURLs(claimants);
 
     const createDistributorTokenAccount = new TransactionInstruction({
         programId: SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
@@ -336,6 +369,7 @@ export const Create = (
     }
 
     console.log("Distributing claim URLs");
+    const sender = setupSender(commMethod, commAuth, commSource);
     for (let idx = 0; idx < claimants.length; ++idx) {
       await sender(claimants[idx], mintUrl);
     }
@@ -480,7 +514,7 @@ export const Create = (
           try {
             await submit(e);
           } catch (err) {
-            setClaimURLs([]);
+            // setClaimURLs([]);
             notify({
               message: "Create failed",
               description: `${err}`,
@@ -495,12 +529,9 @@ export const Create = (
     </Button>
   );
 
-  const encodedClaimURLs = () => {   // TODO: less manual?
-    const encoded = claimURLs.map(url => encodeURIComponent(`"${url}"`));
-    const delim = encodeURIComponent(",");
-    const left = encodeURIComponent("[");
-    const right = encodeURIComponent("]");
-    return `data:text/plain;charset=utf-8,${left}${encoded.join(delim)}${right}`;
+  const encodedClaimURLs = () => {
+    const encoded = encodeURIComponent(JSON.stringify(claimURLs));
+    return `data:text/plain;charset=utf-8,${encoded}`;
   };
 
   return (
