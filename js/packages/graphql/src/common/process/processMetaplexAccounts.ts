74a809d15586e93b71dbff5741e925b090e82eec
Efremov Alexey <lexich121@gmail.com>
2021-10-25 16:49:06 +0300

    tests: specs for processMetaplexAccounts

--- js/packages/graphql/src/common/process/processMetaplexAccounts.ts
+++ js/packages/graphql/src/common/process/processMetaplexAccounts.ts
@@ -11,60 +11,6 @@ import {
 import { METAPLEX_ID, AccountInfoOwnerString } from '../utils';
 import { createProcessor, createPipeline } from './utils';
 
-/*
-export const processMetaplexAccounts: ProcessAccountsFunc = async (
-  { account, pubkey },
-  setter,
-) => {
-  if (!isMetaplexAccount(account)) return;
-
-  try {
-    if (
-      isAuctionManagerV1Account(account) ||
-      isAuctionManagerV2Account(account)
-    ) {
-      const auctionManager = decodeAuctionManager(account.data, pubkey);
-      await setter('auctionManagers', pubkey, auctionManager);
-    }
-
-    if (
-      isBidRedemptionTicketV1Account(account) ||
-      isBidRedemptionTicketV2Account(account)
-    ) {
-      const ticket = decodeBidRedemptionTicket(account.data, pubkey);
-      await setter('bidRedemptions', pubkey, ticket);
-    }
-
-    if (isPayoutTicketV1Account(account)) {
-      const ticket = decodePayoutTicket(account.data, pubkey);
-      await setter('payoutTickets', pubkey, ticket);
-    }
-
-    if (isPrizeTrackingTicketV1Account(account)) {
-      const ticket = decodePrizeTrackingTicket(account.data, pubkey);
-      await setter('prizeTrackingTickets', pubkey, ticket);
-    }
-
-    if (isSafetyDepositConfigV1Account(account)) {
-      const config = decodeSafetyDepositConfig(account.data, pubkey);
-      await setter('safetyDepositConfigs', pubkey, config);
-    }
-
-    if (isStoreV1Account(account)) {
-      const store = decodeStore(account.data, pubkey);
-      await setter('stores', pubkey, store);
-    }
-
-    if (isWhitelistedCreatorV1Account(account)) {
-      const creator = decodeWhitelistedCreator(account.data, pubkey);
-      await setter('creators', pubkey, creator);
-    }
-  } catch (err) {
-    logger.warn(err);
-  }
-};
-*/
-
 const isMetaplexAccount = (account: AccountInfoOwnerString<Buffer>) =>
   account.owner === METAPLEX_ID;
 
@@ -104,37 +50,51 @@ export const METAPLEX_ACCOUNTS_PROCESSOR = createPipeline(
   {
     auctionManagers: createProcessor(
       acc => isAuctionManagerV1Account(acc) || isAuctionManagerV2Account(acc),
-      ({ account, pubkey }) => decodeAuctionManager(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodeAuctionManager(account.data, pubkey);
+      },
     ),
     bidRedemptions: createProcessor(
       acc =>
         isBidRedemptionTicketV1Account(acc) ||
         isBidRedemptionTicketV2Account(acc),
-      ({ account, pubkey }) => decodeBidRedemptionTicket(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodeBidRedemptionTicket(account.data, pubkey);
+      },
     ),
 
     payoutTickets: createProcessor(
       acc => isPayoutTicketV1Account(acc),
-      ({ account, pubkey }) => decodePayoutTicket(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodePayoutTicket(account.data, pubkey);
+      },
     ),
 
     prizeTrackingTickets: createProcessor(
       acc => isPrizeTrackingTicketV1Account(acc),
-      ({ account, pubkey }) => decodePrizeTrackingTicket(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodePrizeTrackingTicket(account.data, pubkey);
+      },
     ),
 
     safetyDepositConfigs: createProcessor(
       acc => isSafetyDepositConfigV1Account(acc),
-      ({ account, pubkey }) => decodeSafetyDepositConfig(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodeSafetyDepositConfig(account.data, pubkey);
+      },
     ),
 
     stores: createProcessor(
       acc => isStoreV1Account(acc),
-      ({ account, pubkey }) => decodeStore(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodeStore(account.data, pubkey);
+      },
     ),
     creators: createProcessor(
       acc => isWhitelistedCreatorV1Account(acc),
-      ({ account, pubkey }) => decodeWhitelistedCreator(account.data, pubkey),
+      ({ account, pubkey }) => {
+        return decodeWhitelistedCreator(account.data, pubkey);
+      },
     ),
   },
   isMetaplexAccount,
