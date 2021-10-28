cd1882ca9dee67789fba18fa5be0ed332653d1ee
Anatolii Serbit <outlawcybercrimes@gmail.com>
2021-10-28 20:59:07 +0300

    Updates

---
+++ _next/static/EvUbIIBEEVy2dgSKYXvZu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-83bbb6132d84784b00f9.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
