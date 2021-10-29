e9c9c0406cf551381b1051eb69912e603898abe6
Lawrence <lawrencejwu@gmail.com>
2021-10-29 12:59:55 -0500

    Pass candy machine authority transfer args on distributor close

--- js/packages/gumdrop/src/components/Close.tsx
+++ js/packages/gumdrop/src/components/Close.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import {
   Button,
+  FormControl,
+  InputLabel,
   Link as HyperLink,
+  MenuItem,
   Stack,
+  Select,
   TextField,
 } from "@mui/material";
 
@@ -27,7 +31,10 @@ import {
   Connection,
 } from "../contexts";
 import {
+  CANDY_MACHINE_ID,
   MERKLE_DISTRIBUTOR_ID,
+  getCandyConfig,
+  getCandyMachineAddress,
   notify,
 } from "../utils";
 
@@ -36,6 +43,9 @@ export const Close = () => {
   const wallet = useWallet();
 
   const [baseKey, setBaseKey] = React.useState("");
+  const [claimMethod, setClaimMethod] = React.useState(localStorage.getItem("claimMethod") || "transfer");
+  const [candyConfig, setCandyConfig] = React.useState(localStorage.getItem("candyConfig") || "");
+  const [candyUUID, setCandyUUID] = React.useState(localStorage.getItem("candyUUID") || "");
 
   const submit = async (e : React.SyntheticEvent) => {
     e.preventDefault();
@@ -62,6 +72,20 @@ export const Close = () => {
       MERKLE_DISTRIBUTOR_ID
     );
 
+    let extraKeys;
+    if (claimMethod === "candy") {
+      const configKey = await getCandyConfig(connection, candyConfig);
+      const [candyMachineKey, ] = await getCandyMachineAddress(
+        configKey, candyUUID);
+
+     extraKeys = [
+            { pubkey: candyMachineKey         , isSigner: false , isWritable: true  } ,
+            { pubkey: CANDY_MACHINE_ID        , isSigner: false , isWritable: false } ,
+      ];
+    } else {
+      extraKeys = [];
+    }
+
     const closeDistributor = new TransactionInstruction({
         programId: MERKLE_DISTRIBUTOR_ID,
         keys: [
@@ -71,6 +95,7 @@ export const Close = () => {
             { pubkey: wallet.publicKey        , isSigner: true  , isWritable: true  } ,
             { pubkey: SystemProgram.programId , isSigner: false , isWritable: false } ,
             { pubkey: TOKEN_PROGRAM_ID        , isSigner: false , isWritable: false } ,
+            ...extraKeys,
         ],
         data: Buffer.from([
           ...Buffer.from(sha256.digest("global:close_distributor")).slice(0, 8),
@@ -106,6 +131,31 @@ export const Close = () => {
     }
   };
 
+  const claimData = (claimMethod) => {
+    if (claimMethod === "candy") {
+      return (
+        <React.Fragment>
+          <TextField
+            style={{width: "60ch"}}
+            id="config-text-field"
+            label="Candy Config"
+            value={candyConfig}
+            onChange={e => setCandyConfig(e.target.value)}
+          />
+          <TextField
+            style={{width: "60ch"}}
+            id="config-uuid-text-field"
+            label="Candy UUID"
+            value={candyUUID}
+            onChange={e => setCandyUUID(e.target.value)}
+          />
+        </React.Fragment>
+      );
+    } else if (claimMethod === "transfer") {
+      return null;
+    }
+  };
+
   return (
     <Stack spacing={2}>
       <TextField
@@ -115,6 +165,24 @@ export const Close = () => {
         value={baseKey}
         onChange={(e) => setBaseKey(e.target.value)}
       />
+      <FormControl fullWidth>
+        <InputLabel id="claim-method-label">Claim Method</InputLabel>
+        <Select
+          labelId="claim-method-label"
+          id="claim-method-select"
+          value={claimMethod}
+          label="Claim Method"
+          onChange={(e) => {
+            localStorage.setItem("claimMethod", e.target.value);
+            setClaimMethod(e.target.value);
+          }}
+          style={{textAlign: "left"}}
+        >
+          <MenuItem value={"transfer"}>Token Transfer</MenuItem>
+          <MenuItem value={"candy"}>Candy Machine</MenuItem>
+        </Select>
+      </FormControl>
+      {claimMethod !== "" && claimData(claimMethod)}
       <Button
         disabled={!wallet.connected || !baseKey}
         variant="contained"
