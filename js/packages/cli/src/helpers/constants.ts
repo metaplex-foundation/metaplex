d6c9ed89b6af3b46950fef9f6927e62659f1e2df
Your Name <you@example.com>
2021-11-02 20:38:55 -0300

    rm comments

--- js/packages/cli/src/helpers/constants.ts
+++ js/packages/cli/src/helpers/constants.ts
@@ -11,7 +11,7 @@ export const ARWEAVE_PAYMENT_WALLET = new PublicKey(
   '6FKvsq4ydWFci6nGq9ckbjYMtnmaqAoatz5c9XWjiDuS',
 );
 export const CANDY_MACHINE_PROGRAM_ID = new PublicKey(
-  'DMtuL3ZWjk9x8a1eam5vMz6ygY77wYUW6oDxUqnTX28T',
+  'cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ',
 );
 export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
   'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
