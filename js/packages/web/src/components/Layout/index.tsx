e740f90bac93b194d60e3cc1dda7890d160ef256
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2021-11-04 17:31:50 -0700

    HOLA-38 pass logo and storefront to app layout and down

--- js/packages/web/src/components/Layout/index.tsx
+++ js/packages/web/src/components/Layout/index.tsx
@@ -1,17 +1,20 @@
+import { Storefront } from '@oyster/common';
 import { Layout } from 'antd';
 import React, { ReactNode } from 'react';
+import { StorefrontNotificationType } from '../../hooks';
 import { AppBar } from '../AppBar';
 
 const { Header, Content } = Layout;
 
 export const AppLayout = React.memo(function AppLayout(props: {
   children?: ReactNode;
+  storefront?: Storefront;
 }) {
   return (
     <>
       <Layout>
         <Header>
-          <AppBar />
+          <AppBar logo={props.storefront?.theme?.logo} />
         </Header>
         <Content id="metaplex-layout-content">{props.children}</Content>
       </Layout>
