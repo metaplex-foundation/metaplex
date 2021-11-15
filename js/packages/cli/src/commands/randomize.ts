31a5b5f7a81f9f3b0070031b0e2ed210a7fe6930
Jake <jjohnson11096@gmail.com>
2021-11-14 23:59:46 -0700

    randomize cmd working

--- js/packages/cli/src/commands/randomize.ts
+++ js/packages/cli/src/commands/randomize.ts
@@ -1,7 +1,9 @@
-import { EXTENSION_PNG } from '../helpers/constants';
+import { EXTENSION_JSON, EXTENSION_PNG } from '../helpers/constants';
 import path from 'path';
 import fs from 'fs';
+import { random } from 'lodash';
 
+// use Fisher-Yates shuffle algorithm
 export async function randomize(
   files: string[],
   totalNFTs: number,
@@ -9,13 +11,49 @@ export async function randomize(
   let randomizeSuccessful = true;
 
   const images = files.filter(val => path.extname(val) === EXTENSION_PNG);
+  const metadata = files.filter(val => path.extname(val) === EXTENSION_JSON);
+
   const SIZE = images.length;
-  /*files.forEach(f => {
-    if (!seen[f.replace(EXTENSION_PNG, '').split('/').pop()]) {
-      seen[f.replace(EXTENSION_PNG, '').split('/').pop()] = true;
-      newFiles.push(f);
+  
+  let currentIndex = images.length, randomIndex;
+
+  // temp filenames
+  const tmpPng = path.dirname(images[0]) + '/tmp' + EXTENSION_PNG;
+  const tmpJson = path.dirname(metadata[0]) + '/tmp' + EXTENSION_JSON;
+
+  // while there remains elements to shuffle
+  while(currentIndex != 0) {
+
+    // get random element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // make sure random and current aren't the same. Making it possible for
+    // them to be same so that we follow the Fisher-Yates algorithm
+    if(randomIndex!=currentIndex){
+
+      const currentPng = images[currentIndex];
+      const currentJson = metadata[currentIndex];
+      const randomPng = images[randomIndex];
+      const randomJson = metadata[randomIndex];
+
+      //
+      // Swap with current
+      //
+      // rename current index png and json filenames to tmp so we can 
+      // swap the random filename with it
+      fs.renameSync(currentPng, tmpPng);
+      fs.renameSync(currentJson, tmpJson);
+
+      // rename random index to current index
+      fs.renameSync(randomPng, currentPng);
+      fs.renameSync(randomJson, currentJson);
+      
+      // rename tmp to random index
+      fs.renameSync(tmpPng, randomPng);
+      fs.renameSync(tmpJson, randomJson);
     }
-  });*/
+  }
 
   console.log('#####HEEEEYYYY######');
 
