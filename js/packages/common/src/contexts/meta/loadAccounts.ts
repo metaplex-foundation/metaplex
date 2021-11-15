ee84459f20defd155922d96b7f8de5f032e06658
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2021-11-15 17:52:43 +0200

    refactor: remove unneded fetch

--- js/packages/common/src/contexts/meta/loadAccounts.ts
+++ js/packages/common/src/contexts/meta/loadAccounts.ts
@@ -891,14 +891,12 @@ export const loadAccounts = async (connection: Connection) => {
     pullMetadataByCreators(connection, state, updateState);
   const loadEditions = () =>
     pullEditions(connection, updateState, state, state.metadata);
-  const loadPacks = () => pullPacks(connection, state);
 
   const loading = [
     loadCreators().then(loadMetadata).then(loadEditions),
     loadVaults(),
     loadAuctions(),
     loadMetaplex(),
-    loadPacks().then(() => {}),
   ];
 
   const done = timeStart('loadAccounts#Promise.all');
