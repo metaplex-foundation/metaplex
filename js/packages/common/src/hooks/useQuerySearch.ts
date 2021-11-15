0ad1507aa169c0c4d0a63c10c93c71a7367b457e
0xCryptoSheik <93099369+0xCryptoSheik@users.noreply.github.com>
2021-11-15 14:03:45 +0100

    fix(connection): ensure stable network connection + refactor ConnectionContext (#958)
    - changing the Solana "network" endpoint now reloads the app fully to avoid possible side-effects, race-conditions, bugs, ...
    - the "network" endpoint is now determined only once on app mount, by priority:
      - from the "network" hash-router querystring param
      - from the "network" local storage key/value
      - default (mainnet-beta / api.metaplex.solana.com)
    - useQuerySearch hook is now properly memoized to prevent unnecessary rendering of its consumers, if the location from react-router hash-router changes, but not the search property.
    - ConnectionContext no longer triggers unnecessary re-renders of its consumers thanks to a memoized provider value.
    - ConnectionContext now only exposes { connection: Connection, endpoint: Endpoint, tokens: Map<string, TokenInfo> }, redundant values have been removed & migrated.
    - general code-cleanup of packages/common/contexts/connection (types, naming, ...).
    - miscellaneous changes (prettier, trivial types, ...)

--- js/packages/common/src/hooks/useQuerySearch.ts
+++ js/packages/common/src/hooks/useQuerySearch.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export function useQuerySearch() {
-  return new URLSearchParams(useLocation().search);
+  const { search } = useLocation();
+  return useMemo(() => new URLSearchParams(search), [search]);
 }
--- js/packages/common/src/hooks/useTokenName.ts
+++ js/packages/common/src/hooks/useTokenName.ts
@@ -3,8 +3,8 @@ import { useConnectionConfig } from '../contexts/connection';
 import { getTokenName } from '../utils/utils';
 
 export function useTokenName(mintAddress?: string | PublicKey) {
-  const { tokenMap } = useConnectionConfig();
+  const { tokens } = useConnectionConfig();
   const address =
     typeof mintAddress === 'string' ? mintAddress : mintAddress?.toBase58();
-  return getTokenName(tokenMap, address);
+  return getTokenName(tokens, address);
 }
