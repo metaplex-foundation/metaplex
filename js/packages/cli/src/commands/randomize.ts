b67b33853c740f1785fe6823ebda8daf7ab9ad01
Jake <jjohnson11096@gmail.com>
2021-11-15 11:42:39 -0700

    adding comments

--- js/packages/cli/src/commands/randomize.ts
+++ js/packages/cli/src/commands/randomize.ts
@@ -2,34 +2,38 @@ import { EXTENSION_JSON, EXTENSION_PNG } from '../helpers/constants';
 import path from 'path';
 import fs from 'fs';
 
-// use Fisher-Yates shuffle algorithm
+// This funciton uses the Fisher-Yates shuffle algorithm to shuffle
+// the png/json pairs in a given array of filenames
 export async function randomize(files: string[]): Promise<boolean> {
   const randomizeSuccessful = true;
 
+  // get images and metadata filenames
   const images = files.filter(val => path.extname(val) === EXTENSION_PNG);
   const metadata = files.filter(val => path.extname(val) === EXTENSION_JSON);
 
-  let currentIndex = images.length,
-    randomIndex;
-
-  // temp filenames
+  // create temp filenames for when we 'shuffle/swap' filenames
   const tmpPng = path.dirname(images[0]) + '/tmp' + EXTENSION_PNG;
   const tmpJson = path.dirname(metadata[0]) + '/tmp' + EXTENSION_JSON;
+
+  // create indexes to use for shuffle algorithm
+  let currentIndex = images.length,
+    randomIndex;
 
   // while there remains elements to shuffle
   while (currentIndex != 0) {
     // get random element
     randomIndex = Math.floor(Math.random() * currentIndex);
+
+    // cycle through indices
     currentIndex--;
 
-    // make sure random and current aren't the same. Making it possible for
-    // them to be same so that we follow the Fisher-Yates algorithm
+    // Swap the random index and current index file names.
+    // Only swap if random chosen index is different than current.
+    // Otherwise it will cause issues when we rename with temp name.
     if (randomIndex != currentIndex) {
-      //
-      // Swap with current
-      //
-      // rename current index png and json filenames to tmp so we can
-      // swap the random filename with it
+      // rename current index filenames to tmp so we can
+      // change the random index filenames to the current index filenames
+      // without duplicating the filename in the filesystem.
       fs.renameSync(images[currentIndex], tmpPng);
       fs.renameSync(metadata[currentIndex], tmpJson);
 
@@ -42,8 +46,6 @@ export async function randomize(files: string[]): Promise<boolean> {
       fs.renameSync(tmpJson, metadata[randomIndex]);
     }
   }
-
-  console.log('#####HEEEEYYYY######');
 
   return randomizeSuccessful;
 }
