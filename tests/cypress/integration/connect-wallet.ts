bdba189ec107c9b77e9f4d7481830c3ffebc5c11
Thorsten Lorenz <thlorenz@gmx.de>
2021-11-16 13:33:50 -0600

    test: setup connect-wallet flow

---
+++ tests/cypress/integration/connect-wallet.ts
@@ -0,0 +1,39 @@
+import { PhantomWalletMock, WindowWithPhanWalletMock } from 'phan-wallet-mock'
+
+let wallet: PhantomWalletMock
+beforeEach(() => {
+  cy.visit('/')
+  cy.window().then((win: WindowWithPhanWalletMock) => {
+    wallet = win.solana
+  })
+})
+
+describe(`Connects Wallet via 'Connect Wallet' menu button -> Connect to Phantom`, () => {
+  it('finds button, connects wallet via click and loads', () => {
+    expect(wallet.isConnected, 'wallet is not connected').false
+    cy.get('#desktop-navbar')
+      .find('.connector')
+      .click()
+      .get('.phantom-button')
+      .click()
+      .then(() => expect(wallet.isConnected, 'wallet is connected').true)
+      .get('div')
+      .contains(/Connected to wallet/i)
+      .get('.loader-container.active')
+  })
+})
+
+describe(`Connects Wallet via 'Connect to configure store'`, () => {
+  it('finds button and connects wallet via click', () => {
+    expect(wallet.isConnected, 'wallet is not connected').false
+    cy.get('button')
+      .contains('Connect')
+      .click()
+      .get('.phantom-button')
+      .click()
+      .then(() => expect(wallet.isConnected, 'wallet is connected').true)
+      .get('div')
+      .contains(/Connected to wallet/i)
+      .get('.loader-container.active')
+  })
+})
---
+++ tests/cypress/support/index.ts
@@ -0,0 +1,31 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import './routes'
+
+import { Keypair } from '@solana/web3.js'
+import { initWalletMockProvider, PhantomWalletMock } from 'phan-wallet-mock'
+
+Cypress.on('window:before:load', (win) => {
+  initWalletMockProvider(win)
+})
+
+before(() => {})
+
+beforeEach(() => {
+  // Cypress commands you would like to run before every single Cypress test.
+})
