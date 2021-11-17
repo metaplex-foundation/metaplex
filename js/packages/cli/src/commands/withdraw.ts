f55bd0f77019008fa56f4d58b09ab6af8ac6d44d
Jarett Dunn <jarettrsdunn+git@gmail.com>
2021-11-17 12:04:13 -0400

    Create withdraw.ts

---
+++ js/packages/cli/src/commands/withdraw.ts
@@ -0,0 +1,52 @@
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
+import {
+  getCandyMachineAddress,
+  getMasterEdition,
+  getMetadata,
+  getTokenWallet,
+  loadCandyProgram,
+  loadWalletKey,
+  uuidFromConfigPubkey,
+  getConfig,
+} from '../helpers/accounts';
+import {
+  TOKEN_METADATA_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+  CANDY_MACHINE_PROGRAM_ID,
+  CANDY_MACHINE,
+} from '../helpers/constants';
+import * as anchor from '@project-serum/anchor';
+import { MintLayout, Token } from '@solana/spl-token';
+import { createAssociatedTokenAccountInstruction } from '../helpers/instructions';
+import { sendTransactionWithRetryWithKeypair } from '../helpers/transactions';
+
+export async function withdraw(
+  keypair: string,
+  env: string,
+  configAddress: PublicKey,
+  charityAddress: PublicKey,
+  lamports: number
+): Promise<string> {
+  const userKeyPair = loadWalletKey(keypair);
+  const anchorProgram = await loadCandyProgram(userKeyPair, env);
+  const signers = [userKeyPair];
+
+    
+  const instructions = [
+    await anchorProgram.instruction.withdrawFunds({
+      accounts: {
+        config: configAddress,
+        authority: userKeyPair.publicKey
+      }
+    )]
+  ];
+  return (
+    await sendTransactionWithRetryWithKeypair(
+      anchorProgram.provider.connection,
+      userKeyPair,
+      instructions,
+      signers,
+    )
+  ).txid;
+}
+0
