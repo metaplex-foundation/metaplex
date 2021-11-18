3a25a751ad62292d7be93a5021c7c29898c92a2f
Jarett Dunn <jarettrsdunn+git@gmail.com>
2021-11-18 12:32:26 -0400

    Update withdraw.ts

--- js/packages/cli/src/commands/withdraw.ts
+++ js/packages/cli/src/commands/withdraw.ts
@@ -25,21 +25,51 @@ export async function withdraw(
   env: string,
   configAddress: PublicKey,
   charityAddress: PublicKey,
-  lamports: number
+  lamports: number,
+  devPercent: number,
+  charityPercent: number
 ): Promise<string> {
   const userKeyPair = loadWalletKey(keypair);
   const anchorProgram = await loadCandyProgram(userKeyPair, env);
   const signers = [userKeyPair];
 
     
-  const instructions = [
+  let instructions = [
     await anchorProgram.instruction.withdrawFunds({
       accounts: {
         config: configAddress,
         authority: userKeyPair.publicKey
       }
     )]
- 
+ if (parseFloat(charityPercent) > 0){
+
+      log.info('Speaking of newfound wealth, this is where you donate % to charity :) the charity address and % are configurable.')
+
+      const tBN = new anchor.BN(lamports);
+
+      const lamportsToCharity =
+            tBN
+        .div(new anchor.BN(100))
+        .mul(new anchor.BN(parseFloat(charityPercent)))
+
+        .toNumber();
+
+      console.log(`Sending ${lamportsToCharity} / ${t} lamports to charity`);
+
+      instructions.puah(
+        
+        
+        
+          await anchor.web3.SystemProgram.transfer({
+            fromPubkey: walletKeyPair.publicKey,
+            toPubkey: new PublicKey(charityAddress),
+            lamports: lamportsToCharity
+          })
+        )
+        
+      );
+
+      log.info('send_to_charity finished', tx)
   return (
     await sendTransactionWithRetryWithKeypair(
       anchorProgram.provider.connection,
