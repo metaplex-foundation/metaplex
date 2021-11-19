3629640c1ca80a82c1bd882cca7968fdff3b0a24
Irina Kadushkina <69786983+ikadushkina@users.noreply.github.com>
2021-11-19 13:48:39 +0300

    feat: redesign pack card (#42)
    * feat: updated pack card
    
    * fix: comments fixed

--- js/packages/web/src/components/PackCard/index.tsx
+++ js/packages/web/src/components/PackCard/index.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo, ReactElement } from 'react';
+import React, { ReactElement, useCallback, useMemo } from 'react';
 import { Card } from 'antd';
-import { useMeta } from '@oyster/common';
+import { shortenAddress, useMeta } from '@oyster/common';
 
 import { MetaAvatar } from '../MetaAvatar';
 import { ArtContent } from '../ArtContent';
 import { getCreator } from './utils';
+import { useArt } from '../../hooks';
+import { ArtType } from '../../types';
 
 interface Props {
   voucherMetadata: string;
@@ -24,41 +26,57 @@ const PackCard = ({
   artView,
 }: Props): ReactElement => {
   const { whitelistedCreatorsByCreator } = useMeta();
+  const art = useArt(voucherMetadata);
 
   const creator = useMemo(
     () => getCreator(whitelistedCreatorsByCreator, authority),
     [whitelistedCreatorsByCreator, authority],
   );
 
-  const packStatusTitle = cardsRedeemed ? 'Opened Pack' : 'Sealed Pack';
+  const packStatusTitle = cardsRedeemed ? 'Opened' : 'Sealed';
   const headingTitle = artView ? packStatusTitle : 'Pack';
+  const canBeReveal = allowedAmountToRedeem && cardsRedeemed ?
+    allowedAmountToRedeem - cardsRedeemed : allowedAmountToRedeem || 0;
 
-  const infoMessageTitle = artView ? 'NFTs' : 'PACK OPENING UNLOCKS';
-  const openedNumber =
-    cardsRedeemed && cardsRedeemed > 0
-      ? `${cardsRedeemed} of ${allowedAmountToRedeem} opened`
-      : `${allowedAmountToRedeem} unopened`;
-  const infoMessage = artView ? openedNumber : `Nov 9, 2021`;
+  const infoMessageTitle = artView ? `${canBeReveal} NFT reveal left` : 'PACK OPENING UNLOCKS';
+
+  const showBadge = useCallback(() => {
+    switch (art.type) {
+      case ArtType.NFT: return 'Unique';
+      case ArtType.Master: return 'NFT 0';
+      case ArtType.Print: return `${art.edition} of ${art.supply}`;
+     }
+  }, [art])
 
   return (
     <Card hoverable className="auction-render-card" bordered={false}>
       <div className="card-art-info">
+        <div className="pack-gray-wrapper">
         <div className="card-artist-info card-artist-info--pack">
-          <MetaAvatar creators={[creator]} />
+          <div className="pack-creator-info">
+            <MetaAvatar creators={[creator]} />
+            <span className="pack-creator-name">{creator.name || shortenAddress(creator?.address || '')}</span>
+          </div>
           <div className="card-artist-info__subtitle">
-            <p>{headingTitle}</p>
+            <p className="info-message__main">{showBadge()}</p>
           </div>
         </div>
         <div className="art-content-wrapper">
           <ArtContent pubkey={voucherMetadata} preview={false} />
         </div>
         <div className="art-name">{name}</div>
+        </div>
         <div className="art-auction-info">
-          <span className="info-message">{infoMessageTitle}</span>
-          <p className="info-message__main">{infoMessage}</p>
+          <div className="art-auction-info__left-side">
+            <img src="/grid-4.svg" />
+            <span className="info-message">{infoMessageTitle}</span>
+          </div>
+          <div className="art-auction-info__right-side">
+            <span>{headingTitle}</span>
+          </div>
         </div>
       </div>
-      {!artView && <div className="card-bid-info"></div>}
+      {!artView && <div className="card-bid-info"/>}
     </Card>
   );
 };
