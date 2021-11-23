226a5fbf0192a0dd5652bc47832443074eefff06
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-11-23 13:51:24 +0100

    feat: update halluce logo

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -50,7 +50,7 @@ const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/metaplex-logo.svg'} />}
+          title={<img src={'/halluce-logo.svg'} />}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -109,7 +109,7 @@ const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/metaplex-logo.svg'} />
+      <img src={'/halluce-logo.svg'} />
     </Link>
   );
 };
