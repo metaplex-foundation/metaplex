c612b4fb8feaa8ef4673decd4e77d06e71d1749f
Lawrence Wu <lawrencejwu@gmail.com>
2021-11-24 11:30:59 -0600

    copy template for recipe claim website

---
+++ js/packages/collectoooooors/src/App.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  BrowserRouter,
+  // Link,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import {
+  Box,
+  // Link as HyperLink,
+  // Stack,
+} from "@mui/material";
+
+import "./App.css";
+import {
+  useColorMode,
+} from "./contexts";
+import { Header } from "./components/Header/Header";
+import { Redeem } from "./components/Redeem";
+
+const About = () => {
+  return (
+    <>
+      About
+    </>
+  );
+};
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+};
+
+// eslint-disable-next-line
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = React.useState(
+    getWindowDimensions()
+  );
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+function App() {
+  const colorModeCtx = useColorMode();
+
+  React.useEffect(() => {}, [colorModeCtx.mode]);
+
+  const theme = React.useMemo(
+    () => {
+      let mode;
+      if (colorModeCtx.mode === "dark" || !colorModeCtx.mode) {
+        mode = "dark";
+      } else {
+        mode = "light";
+      }
+
+      return createTheme({
+        palette: {
+          mode,
+        },
+      })
+    },
+    [colorModeCtx.mode]
+  );
+
+  const { width } = useWindowDimensions();
+
+  return (
+    <div className="App" style={{ backgroundColor: "transparent" }}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <Header narrow={width < 670}/>
+          <Box
+            maxWidth="60ch"
+            width="calc(100% - 60px)"
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Box height="40px" />
+            <Switch>
+              <Route exact path="/collectoooooors/redeem/" component={Redeem} />
+              <Route exact path="/collectoooooors/" component={About} />
+            </Switch>
+            <Box height="80px" />
+          </Box>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ js/packages/collectoooooors/src/utils/ids.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const COLLECTOOOOOORS_PROGRAM_ID = new PublicKey(
+  '9R4RuSGk3raAVf6TJ7o1ixywcDW89p1MSo2ns9LEBXNK',
+);
+
+export const COLLECTOOOOOORS_PREFIX = Buffer.from(
+  'collectoooooors'
+);
+
+export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+);
+
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+);
