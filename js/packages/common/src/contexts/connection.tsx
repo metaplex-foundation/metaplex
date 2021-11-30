b80a9ee5ace50a0a3a2c0a317cea97e083c5e479
steveluscher <github@steveluscher.com>
2021-11-29 22:55:59 -0800

    fix: tidy async promise executor lint

--- js/packages/common/src/contexts/connection.tsx
+++ js/packages/common/src/contexts/connection.tsx
@@ -786,71 +786,68 @@ async function awaitTransactionSignatureConfirmation(
     err: null,
   };
   let subId = 0;
-  status = await new Promise(async (resolve, reject) => {
+  status = await (async (): Promise<SignatureStatus | null | void> => {
     setTimeout(() => {
       if (done) {
         return;
       }
       done = true;
       console.log('Rejecting for timeout...');
-      reject({ timeout: true });
+      throw { timeout: true };
     }, timeout);
     try {
-      subId = connection.onSignature(
-        txid,
-        (result, context) => {
-          done = true;
-          status = {
-            err: result.err,
-            slot: context.slot,
-            confirmations: 0,
-          };
-          if (result.err) {
-            console.log('Rejected via websocket', result.err);
-            reject(status);
-          } else {
-            console.log('Resolved via websocket', result);
-            resolve(status);
-          }
-        },
-        commitment,
-      );
+      return await new Promise((resolve, reject) => {
+        subId = connection.onSignature(
+          txid,
+          (result, context) => {
+            done = true;
+            const nextStatus = {
+              err: result.err,
+              slot: context.slot,
+              confirmations: 0,
+            };
+            if (result.err) {
+              console.log('Rejected via websocket', result.err);
+              reject(nextStatus);
+            } else {
+              console.log('Resolved via websocket', result);
+              resolve(nextStatus);
+            }
+          },
+          commitment,
+        );
+      });
     } catch (e) {
       done = true;
       console.error('WS error in setup', txid, e);
     }
     while (!done && queryStatus) {
-      // eslint-disable-next-line no-loop-func
-      (async () => {
-        try {
-          const signatureStatuses = await connection.getSignatureStatuses([
-            txid,
-          ]);
-          status = signatureStatuses && signatureStatuses.value[0];
-          if (!done) {
-            if (!status) {
-              console.log('REST null result for', txid, status);
-            } else if (status.err) {
-              console.log('REST error for', txid, status);
-              done = true;
-              reject(status.err);
-            } else if (!status.confirmations) {
-              console.log('REST no confirmations for', txid, status);
-            } else {
-              console.log('REST confirmation for', txid, status);
-              done = true;
-              resolve(status);
-            }
-          }
-        } catch (e) {
-          if (!done) {
-            console.log('REST connection error: txid', txid, e);
+      try {
+        const signatureStatuses = await connection.getSignatureStatuses([txid]);
+        const nextStatus = signatureStatuses && signatureStatuses.value[0];
+        if (!done) {
+          if (!nextStatus) {
+            console.log('REST null result for', txid, nextStatus);
+          } else if (nextStatus.err) {
+            console.log('REST error for', txid, nextStatus);
+            done = true;
+            throw nextStatus.err;
+          } else if (!nextStatus.confirmations) {
+            console.log('REST no confirmations for', txid, nextStatus);
+          } else {
+            console.log('REST confirmation for', txid, nextStatus);
+            done = true;
+            return nextStatus;
           }
         }
-      })();
+      } catch (e) {
+        if (!done) {
+          console.log('REST connection error: txid', txid, e);
+        }
+      }
       await sleep(2000);
     }
-  });
+  })();
 
   //@ts-ignore
   if (connection._signatureSubscriptions[subId])
