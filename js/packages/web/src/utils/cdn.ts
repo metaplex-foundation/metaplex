bf408d314a7a13d77b8f587941c1d28c08c7f4d5
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-12-13 13:29:54 -0800

    feat(artwork): support ipfs holaplex cache

--- js/packages/web/src/utils/cdn.ts
+++ js/packages/web/src/utils/cdn.ts
@@ -1,14 +1,19 @@
 const ARWEAVE_CDN = process.env.NEXT_PUBLIC_ARWEAVE_CDN;
+const IPFS_CDN = process.env.NEXT_PUBLIC_IPFS_CDN;
 
 export const maybeCDN = (uri: string) => {
-  if (ARWEAVE_CDN) {
+  if (ARWEAVE_CDN && uri.includes('arweave.net')) {
     const res = uri.replace(
       /https:\/\/(www.)?arweave\.net(:443)?/g,
       ARWEAVE_CDN,
     );
 
     return res;
-  }
+  } else if (IPFS_CDN && uri.includes('ipfs.dweb')) {
+    const res = uri.replace(/https:\/\/(.*).ipfs.dweb.*$/g, `${IPFS_CDN}/$1`);
 
-  return uri;
+    return res;
+  } else {
+    return uri;
+  }
 };
