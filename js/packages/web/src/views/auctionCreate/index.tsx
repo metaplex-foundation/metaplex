497bc7f57bff91a13c02f32c0f78b326ea5fda8b
sjc <email@sjc.sh>
2021-12-13 18:14:30 -0600

    Clear listing attributes when user selects diff listing type

--- js/packages/web/src/views/auctionCreate/index.tsx
+++ js/packages/web/src/views/auctionCreate/index.tsx
@@ -135,7 +135,7 @@ export const AuctionCreateView = () => {
   const mint = useMint(QUOTE_MINT);
   const { width } = useWindowDimensions();
   const [percentComplete, setPercentComplete] = useState(0);
-  const [rejection, setRejection] = useState<string>()
+  const [rejection, setRejection] = useState<string>();
   const [step, setStep] = useState<number>(1);
   const [stepsVisible, setStepsVisible] = useState<boolean>(true);
   const [auctionObj, setAuctionObj] = useState<
@@ -146,7 +146,8 @@ export const AuctionCreateView = () => {
       }
     | undefined
   >(undefined);
-  const [attributes, setAttributes] = useState<AuctionState>({
+
+  const freshAttributes: AuctionState = {
     reservationPrice: 0,
     items: [],
     category: AuctionCategory.Open,
@@ -155,7 +156,9 @@ export const AuctionCreateView = () => {
     winnersCount: 1,
     startSaleTS: undefined,
     startListTS: undefined,
-  });
+  };
+
+  const [attributes, setAttributes] = useState<AuctionState>(freshAttributes);
 
   const [tieredAttributes, setTieredAttributes] = useState<TieredAuctionState>({
     items: [],
@@ -176,8 +179,8 @@ export const AuctionCreateView = () => {
       },
       {
         programId: VAULT_ID,
-        processAccount: processVaultData
-      }
+        processAccount: processVaultData,
+      },
     );
   }, [connection]);
 
@@ -193,7 +196,9 @@ export const AuctionCreateView = () => {
 
   const createAuction = async () => {
     let winnerLimit: WinnerLimit;
-    let auctionInfo: { vault: string, auction: string, auctionManager: string } | undefined;
+    let auctionInfo:
+      | { vault: string; auction: string; auctionManager: string }
+      | undefined;
 
     if (
       attributes.category === AuctionCategory.InstantSale &&
@@ -500,7 +505,7 @@ export const AuctionCreateView = () => {
     const participationSafetyDepositDraft = isOpenEdition
       ? attributes.items[0]
       : attributes.participationNFT;
-    
+
     try {
       auctionInfo = await createAuctionManager(
         connection,
@@ -517,11 +522,10 @@ export const AuctionCreateView = () => {
     } catch (e: any) {
       setRejection(e.message);
       Bugsnag.notify(e);
-      return;  
+      return;
     }
 
     try {
-
       track('new_listing', {
         category: 'creation',
         label: isInstantSale ? 'instant sale' : 'auction',
@@ -540,10 +544,12 @@ export const AuctionCreateView = () => {
   const categoryStep = (
     <CategoryStep
       confirm={(category: AuctionCategory) => {
-        setAttributes({
-          ...attributes,
-          category,
-        });
+        if (category !== attributes.category) {
+          setAttributes({
+            ...freshAttributes,
+            category,
+          });
+        }
         gotoNextStep();
       }}
     />
