daded169f2151fcc4247347adb086ee1070ce143
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2021-12-29 10:22:53 +0200

    chore: revert configs

--- js/packages/common/src/utils/ids.ts
+++ js/packages/common/src/utils/ids.ts
@@ -66,16 +66,16 @@ export const MEMO_ID = new PublicKey(
 );
 
 export const METADATA_PROGRAM_ID =
-  '97N7MM9VDDeJmk9CH9wKCQG3dn1wgpGaedYbq5Vey17c' as StringPublicKey;
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s' as StringPublicKey;
 
 export const VAULT_ID =
   'vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn' as StringPublicKey;
 
 export const AUCTION_ID =
-  '9qyd7zhgK8nTGF2mnjLzzo3Z4NRLH35o4Q69PnaL3tJ' as StringPublicKey;
+  'auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8' as StringPublicKey;
 
 export const METAPLEX_ID =
-  '8AqKV7JS4D8s9t5JMJS4cHGcnQT8ZjV994iWb8xWCjyj' as StringPublicKey;
+  'p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98' as StringPublicKey;
 
 export const PACK_CREATE_ID = new PublicKey(
   'packFeFNZzMfD9aVWL7QbGz1WcU7R9zpf6pvNsw2BLu',
