7be25d4a77ff83429a44f50e7d551ad2db730f6e
sjc <email@sjc.sh>
2021-12-31 22:28:00 -0600

    refactor main menu

--- js/packages/web/src/components/MainMenu/MainMenu.tsx
+++ js/packages/web/src/components/MainMenu/MainMenu.tsx
@@ -1,66 +1,64 @@
 import { useStore } from '@oyster/common';
 import { useWallet } from '@solana/wallet-adapter-react';
 import React from 'react';
-import { Link, useMatch, useLocation } from 'react-router-dom';
+import { Link, useResolvedPath, useMatch } from 'react-router-dom';
+import cx from 'classnames';
 
 export const MainMenu = () => {
   const { connected, publicKey } = useWallet();
   const { ownerAddress } = useStore();
 
+  const getMenuItem = (key: string, linkAppend?: string, title?: string) => {
+    return {
+      key,
+      title: title || key[0].toUpperCase() + key.substring(1),
+      link: `/${key + linkAppend || ''}`,
+      group: `/${key}`,
+    };
+  };
+
   let menu = [
-    {
-      key: 'auctions',
-      title: 'Listings',
-      link: '/listings?views=live',
-    },
-    {
-      key: 'creators',
-      title: 'Creators',
-      link: `/creators/${ownerAddress}`,
-      isMatch: useMatch('/creators/:item'),
-    },
+    getMenuItem('listings', '?views=live'),
+    getMenuItem('creators', `/${ownerAddress}`),
   ];
 
   if (connected) {
-    menu = [
-      ...menu,
-      {
-        key: 'owned',
-        title: 'Owned',
-        link: '/owned',
-      },
-    ];
+    menu = [...menu, getMenuItem('owned')];
   }
 
   if (publicKey?.toBase58() === ownerAddress) {
-    menu = [
-      ...menu,
-      {
-        key: 'admin',
-        title: 'Admin',
-        link: '/admin',
-      },
-    ];
+    menu = [...menu, getMenuItem('admin')];
   }
+
+  interface NavMenuItemProps {
+    to: string;
+    key: string;
+    group?: string;
+    title: string;
+  }
+
+  const NavMenuItem = ({ to, title, key, group }: NavMenuItemProps) => {
+    const resolved = useResolvedPath(group || to);
+    const match = useMatch({ path: resolved.pathname, end: false });
+
+    return (
+      <Link
+        to={to}
+        key={key}
+        className={cx('nav-menu-item', {
+          active: match,
+        })}
+      >
+        {title}
+      </Link>
+    );
+  };
 
   return (
     <div className="nav-menu-wrapper">
-      {menu.map(({ key, link, title, isMatch }) => {
-        return (
-          <Link
-            to={link}
-            key={key}
-            className={
-              'nav-menu-item' +
-              (isMatch || link.startsWith(useLocation().pathname)
-                ? ' active'
-                : '')
-            }
-          >
-            {title}
-          </Link>
-        );
-      })}
+      {menu.map(({ key, title, link, group }) => (
+        <NavMenuItem to={link} key={key} group={group} title={title} />
+      ))}
     </div>
   );
 };
