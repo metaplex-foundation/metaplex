631dc6a21112318226804ac03dece69bd9ff7af3
torinoutacnh <torinoutacnh@gmail.com>
2022-01-02 14:15:32 +0700

    Updates

---
+++ _next/static/MUApsLBqXs1TZ_e92dUGD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-c14ebc950a4833fe5820.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
