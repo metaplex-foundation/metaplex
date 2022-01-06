e09292a1ddacc2c40a1e2c0338f2375ec48283b8
sjc <email@sjc.sh>
2022-01-05 22:52:04 -0600

    implement new designs WIP

---
+++ js/packages/web/src/components/SecondaryMenu/index.tsx
@@ -0,0 +1,26 @@
+import { ConnectButton } from '@oyster/common';
+import { useWallet } from '@solana/wallet-adapter-react';
+import React from 'react';
+import { CurrentUserBadge } from '../CurrentUserBadge';
+import { HowToBuyModal } from '../HowToBuyModal';
+import { Notifications } from '../Notifications';
+
+export const SecondaryMenu = () => {
+  const { connected } = useWallet();
+
+  return (
+    <div className="secondary-menu">
+      {connected ? (
+        <>
+          <Notifications buttonType="text" />
+          <CurrentUserBadge showAddress={true} buttonType="text" />
+        </>
+      ) : (
+        <>
+          <HowToBuyModal buttonType="text" />
+          <ConnectButton type="text" allowWalletChange={false} />
+        </>
+      )}
+    </div>
+  );
+};
