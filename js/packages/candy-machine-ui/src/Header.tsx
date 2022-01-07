351e9276ac7eca12cede32b1a350cc37f6587914
Will Roeder <roederw@outlook.com>
2022-01-07 10:07:01 -0800

    Deprecating Fair Launch Protocol UI (#1424)
    Removed Fair Launch Protocol logic from Candy Machine Mint UI and moved the package from fair-launch to candy-machine-ui

---
+++ js/packages/candy-machine-ui/src/Header.tsx
@@ -0,0 +1,57 @@
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { MintCountdown } from './MintCountdown';
+import { toDate, formatNumber } from './utils';
+import { CandyMachineAccount } from './candy-machine';
+
+type HeaderProps = {
+  candyMachine?: CandyMachineAccount;
+};
+
+export const Header = ({ candyMachine }: HeaderProps) => {
+  return (
+    <Grid container direction="row" justifyContent="center" wrap="nowrap">
+      <Grid container direction="row" wrap="nowrap">
+        {candyMachine && (
+          <Grid container direction="row" wrap="nowrap">
+            <Grid container direction="column">
+              <Typography variant="body2" color="textSecondary">
+                Remaining
+              </Typography>
+              <Typography
+                variant="h6"
+                color="textPrimary"
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >
+                {`${candyMachine?.state.itemsRemaining}`}
+              </Typography>
+            </Grid>
+            <Grid container direction="column">
+              <Typography variant="body2" color="textSecondary">
+                Price
+              </Typography>
+              <Typography
+                variant="h6"
+                color="textPrimary"
+                style={{ fontWeight: 'bold' }}
+              >
+                ◎ {formatNumber.asNumber(candyMachine?.state.price!)}
+              </Typography>
+            </Grid>
+          </Grid>
+        )}
+        <MintCountdown
+          date={toDate(candyMachine?.state.goLiveDate)}
+          style={{ justifyContent: 'flex-end' }}
+          status={
+            !candyMachine?.state?.isActive || candyMachine?.state?.isSoldOut
+              ? 'COMPLETED'
+              : 'LIVE'
+          }
+        />
+      </Grid>
+    </Grid>
+  );
+};
