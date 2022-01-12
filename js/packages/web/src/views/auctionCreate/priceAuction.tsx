050cc5d77641bcbe21eb92c507277c6896a94546
sjc <email@sjc.sh>
2022-01-12 11:51:03 -0600

    update to use ant form validation

--- js/packages/web/src/views/auctionCreate/priceAuction.tsx
+++ js/packages/web/src/views/auctionCreate/priceAuction.tsx
@@ -1,4 +1,5 @@
-import { Button, InputNumber, Space } from 'antd';
+import { Button, InputNumber, Space, Form } from 'antd';
+import { useForm } from 'antd/lib/form/Form';
 import React, { useState } from 'react';
 import { AuctionCategory, AuctionState } from '.';
 
@@ -10,24 +11,12 @@ export const PriceAuction = (props: {
   type InputState = number | null;
   const [price, setPrice] = useState<InputState>(null);
   const [tickSize, setTickSize] = useState<InputState>(null);
-  const [priceError, setPriceError] = useState(false);
+  const [form] = useForm();
 
-  const handlePriceError = (price: InputState, tick: InputState) => {
+  const checkForPriceError = (price: InputState, tick: InputState) => {
     const multiplier = 1000;
-    if (tick && price && (price * multiplier) % (tick * multiplier) !== 0) {
-      setPriceError(true);
-    } else {
-      setPriceError(false);
-    }
-  };
-
-  const handleInput = (input: number, isPrice: boolean) => {
-    if (isPrice) {
-      setPrice(input);
-      handlePriceError(input, tickSize);
-    } else {
-      setTickSize(input);
-      handlePriceError(price, input);
+    if (price && tick && (price * multiplier) % (tick * multiplier) !== 0) {
+      throw new Error();
     }
   };
 
@@ -38,93 +27,147 @@ export const PriceAuction = (props: {
         <p>Set the price for your auction.</p>
       </div>
 
-      {props.attributes.category === AuctionCategory.Open && (
-        <label>
-          <h3>Price</h3>
-          <p>
-            This is the fixed price that everybody will pay for your
-            Participation NFT.
-          </p>
-          <InputNumber<number>
-            min={0}
-            decimalSeparator="."
-            className="metaplex-fullwidth"
-            step="0.01"
-            autoFocus
-            placeholder="Fixed Price in SOL"
-            onChange={price => {
-              handleInput(price, true);
-              props.setAttributes({
-                ...props.attributes,
-                // Do both, since we know this is the only item being sold.
-                participationFixedPrice: price,
-                priceFloor: price,
-              });
-            }}
-            precision={2}
-            formatter={x => (x ? `◎ ${x}` : '')}
-          />
-        </label>
-      )}
-      {props.attributes.category !== AuctionCategory.Open && (
-        <label>
-          <h3>Price Floor</h3>
-          <p>This is the starting bid price for your auction.</p>
-          <InputNumber<number>
-            min={0}
-            decimalSeparator="."
-            className="metaplex-fullwidth"
-            step="0.01"
-            autoFocus
-            placeholder="Price in SOL"
-            onChange={price => {
-              handleInput(price, true);
-              props.setAttributes({
-                ...props.attributes,
-                priceFloor: price,
-              });
-            }}
-            precision={2}
-            formatter={x => (x ? `◎ ${x}` : '')}
-          />
-        </label>
-      )}
-      <label>
-        <h3 className="metaplex-margin-top-4">Tick Size</h3>
-        <p>All bids must fall within this price increment.</p>
-        <InputNumber<number>
-          min={0}
-          decimalSeparator="."
-          className="metaplex-fullwidth"
-          step="0.01"
-          placeholder="Tick size in SOL"
-          onChange={tick => {
-            handleInput(tick, false);
-            props.setAttributes({
-              ...props.attributes,
-              priceTick: tick,
-            });
-          }}
-          precision={2}
-          formatter={x => (x ? `◎ ${x}` : '')}
-        />
-      </label>
-
-      {priceError && (
-        <div className="error-message">
-          Starting price must be an exact multiple of the tick size!
-        </div>
-      )}
-
-      <Button
-        className="metaplex-fullwidth metaplex-margin-top-4 metaplex-margin-bottom-4"
-        type="primary"
-        size="large"
-        onClick={props.confirm}
-        disabled={priceError}
+      <Form
+        form={form}
+        onFinish={() => {
+          form.validateFields(['price', 'tickSize']).then(props.confirm);
+        }}
       >
-        Continue
-      </Button>
+        {props.attributes.category === AuctionCategory.Open && (
+          <Form.Item
+            name="price"
+            rules={[
+              {
+                message:
+                  'Starting price must be an exact multiple of the tick size!',
+                validator: async (_, price) => {
+                  checkForPriceError(price, tickSize);
+                },
+              },
+              {
+                required: true,
+                message: 'Price is required.',
+              },
+            ]}
+          >
+            <label>
+              <h3>Price</h3>
+              <p>
+                This is the fixed price that everybody will pay for your
+                Participation NFT.
+              </p>
+              <div className="metaplex-flex metaplex-align-items-center metaplex-gap-2">
+                ◎
+                <InputNumber<number>
+                  min={0}
+                  decimalSeparator="."
+                  className="metaplex-fullwidth"
+                  step="0.01"
+                  autoFocus
+                  placeholder="Fixed Price in SOL"
+                  onChange={price => {
+                    setPrice(price);
+                    props.setAttributes({
+                      ...props.attributes,
+                      // Do both, since we know this is the only item being sold.
+                      participationFixedPrice: price,
+                      priceFloor: price,
+                    });
+                  }}
+                  precision={2}
+                />
+              </div>
+            </label>
+          </Form.Item>
+        )}
+        {props.attributes.category !== AuctionCategory.Open && (
+          <Form.Item
+            name="price"
+            rules={[
+              {
+                message:
+                  'Starting price must be an exact multiple of the tick size!',
+                validator: async (_, price) => {
+                  checkForPriceError(price, tickSize);
+                },
+              },
+              {
+                required: true,
+                message: 'Price is required.',
+              },
+            ]}
+          >
+            <label>
+              <h3>Price Floor</h3>
+              <p>This is the starting bid price for your auction.</p>
+              <div className="metaplex-flex metaplex-align-items-center metaplex-gap-2">
+                ◎
+                <InputNumber<number>
+                  min={0}
+                  decimalSeparator="."
+                  className="metaplex-fullwidth"
+                  step="0.01"
+                  autoFocus
+                  placeholder="Price in SOL"
+                  onChange={price => {
+                    setPrice(price);
+                    props.setAttributes({
+                      ...props.attributes,
+                      priceFloor: price,
+                    });
+                  }}
+                  precision={2}
+                />
+              </div>
+            </label>
+          </Form.Item>
+        )}
+        <Form.Item
+          name="tickSize"
+          rules={[
+            {
+              message:
+                'Starting price must be an exact multiple of the tick size!',
+              validator: async (_, tickSize) => {
+                checkForPriceError(price, tickSize);
+              },
+            },
+          ]}
+        >
+          <label>
+            <h3 className="metaplex-margin-top-4">Tick Size</h3>
+            <p>All bids must fall within this price increment.</p>
+            <div className="metaplex-flex metaplex-align-items-center metaplex-gap-2">
+              ◎
+              <InputNumber<number>
+                min={0}
+                decimalSeparator="."
+                className="metaplex-fullwidth"
+                step="0.01"
+                placeholder="Tick size in SOL"
+                onChange={tick => {
+                  setTickSize(tick);
+                  props.setAttributes({
+                    ...props.attributes,
+                    priceTick: tick,
+                  });
+                }}
+                precision={2}
+              />
+            </div>
+          </label>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            className="metaplex-fullwidth metaplex-margin-top-4 metaplex-margin-bottom-4"
+            type="primary"
+            size="large"
+            htmlType="submit"
+          >
+            Continue
+          </Button>
+        </Form.Item>
+      </Form>
     </Space>
   );
 };
