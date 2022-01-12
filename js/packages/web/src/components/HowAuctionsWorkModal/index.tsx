cb9d4d7f152f83522748a7fd0874ae7a93c00ae6
atom3000 <atom3000@hotmail.com>
2022-01-12 10:30:35 +0000

    m

--- js/packages/web/src/components/HowAuctionsWorkModal/index.tsx
+++ js/packages/web/src/components/HowAuctionsWorkModal/index.tsx
@@ -18,17 +18,17 @@ export const HowAuctionsWorkModal: React.FC<HowAuctionsWorkModalProps> = ({
         {
           title: 'Placing a Bid',
           description: `Once you find an NFT you’d like to own, place a bid on the auction page. Keep in mind the amount of SOL you bid will be locked in your wallet for the remainder of the auction.`,
-          imgSrc: '/modals/how-auctions-work-1.jpg',
+          imgSrc: 'metaplex/modals/how-auctions-work-1.jpg',
         },
         {
           title: 'Winning an Auction',
           description: `Keep an eye on the auctions page (and your notifications) to know when you’ve been outbid, and how the sale is progressing.`,
-          imgSrc: '/modals/how-auctions-work-2.jpg',
+          imgSrc: 'metaplex/modals/how-auctions-work-2.jpg',
         },
         {
           title: 'Redeeming your NFT',
           description: `If you’re lucky enough to win your NFT auction, you’ll have to redeem it to add it to your wallet. This can be done from the auction, winning notification, or your profile on ${LABELS.STORE_NAME}.`,
-          imgSrc: '/modals/how-auctions-work-3.jpg',
+          imgSrc: 'metaplex/modals/how-auctions-work-3.jpg',
         },
       ]}
     />
