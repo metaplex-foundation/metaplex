18c5fe668184e3b74ce01cdde82b58fe68f5c61b
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-01-13 17:30:30 +0700

    Updates

---
+++ _next/static/IJBOwDFy9njta-_2GCEeD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-9690092b80434fc0bf8d.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
