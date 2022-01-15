4703297c339bcc0ab3b91529da450a1c21d3da17
Will Roeder <roederw@outlook.com>
2022-01-14 17:10:58 -0800

    Updates

---
+++ _next/static/fGzZ6Au5Y4nOAyQR566C4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-7e38a1b7865d43c666d5.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
