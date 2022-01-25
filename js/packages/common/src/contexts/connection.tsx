be156cf2763faa58bc9dba43a034b4f63d5c10ee
Will Roeder <roederw@outlook.com>
2022-01-24 22:02:43 -0800

    Merging Enable lint for React component into masters

--- js/packages/common/src/contexts/connection.tsx
+++ js/packages/common/src/contexts/connection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { getTokenListContainerPromise } from '../utils';
 import { TokenInfo, ENV as ChainId } from '@solana/spl-token-registry';
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
--- js/packages/web/src/contexts/coingecko.tsx
+++ js/packages/web/src/contexts/coingecko.tsx
@@ -20,23 +20,27 @@ export const solToUSD = async (): Promise<number> => {
 };
 
 export const altSplToUSD = async (cgTokenName?: string): Promise<number> => {
-  if (!process.env.NEXT_SPL_TOKEN_MINTS || process.env.NEXT_SPL_TOKEN_MINTS.length == 0)
-    return 0
+  if (
+    !process.env.NEXT_SPL_TOKEN_MINTS ||
+    process.env.NEXT_SPL_TOKEN_MINTS.length == 0
+  )
+    return 0;
 
-  const cg_spl_token_id = cgTokenName? cgTokenName.toLowerCase().split(' ').join('-')
-      : process.env.NEXT_CG_SPL_TOKEN_ID || ""
+  const cg_spl_token_id = cgTokenName
+    ? cgTokenName.toLowerCase().split(' ').join('-')
+    : process.env.NEXT_CG_SPL_TOKEN_ID || '';
   const url = `${COINGECKO_COIN_PRICE_API}?ids=${cg_spl_token_id}&vs_currencies=usd`;
   const resp = await window.fetch(url).then(resp => resp.json());
   //console.log("[--P]Processing", cgTokenName, resp)
   return resp[cg_spl_token_id]?.usd;
 };
 
-const CoingeckoContext =
-  React.createContext<CoingeckoContextState | null>(null);
+const CoingeckoContext = React.createContext<CoingeckoContextState | null>(
+  null,
+);
 export function CoingeckoProvider({ children = null as any }) {
   const [solPrice, setSolPrice] = useState<number>(0);
   const [allSplPrices, setAllSplPrices] = useState<AllSplTokens[]>([]);
-  const tokenList = useTokenList().subscribedTokens
 
   useEffect(() => {
     let timerId = 0;
@@ -44,13 +48,17 @@ export function CoingeckoProvider({ children = null as any }) {
       const solprice = await solToUSD();
       setSolPrice(solprice);
 
-      const subscribedTokenMints = process.env.NEXT_SPL_TOKEN_MINTS? process.env.NEXT_SPL_TOKEN_MINTS.split(","): []
-      const subscribedTokenIDS = process.env.NEXT_CG_SPL_TOKEN_IDS? process.env.NEXT_CG_SPL_TOKEN_IDS.split(","): []
-
+      const subscribedTokenMints = process.env.NEXT_SPL_TOKEN_MINTS
+        ? process.env.NEXT_SPL_TOKEN_MINTS.split(',')
+        : [];
+      const subscribedTokenIDS = process.env.NEXT_CG_SPL_TOKEN_IDS
+        ? process.env.NEXT_CG_SPL_TOKEN_IDS.split(',')
+        : [];
       const splPricePromises: Promise<AllSplTokens | void>[] = [];
-      for (let i=0; i < subscribedTokenMints.length; i++) {
-        const splName = subscribedTokenIDS[i]
-        const splMint = subscribedTokenMints[i]
+
+      for (let i = 0; i < subscribedTokenMints.length; i++) {
+        const splName = subscribedTokenIDS[i];
+        const splMint = subscribedTokenMints[i];
 
         //console.log("[--P]PROCESSING TOKEN",i,  splName, splMint)
         splPricePromises.push(
