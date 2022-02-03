dd98f65829f2d15ccc1b2ea79ba75c58a67c5859
Kasun Peiris <peiris@kasun.io>
2022-02-04 04:15:31 +0530

    Call to action

--- js/packages/web/src/pages/index.tsx
+++ js/packages/web/src/pages/index.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from 'next';
 
 import { Header } from '../components-v2/sections/Header';
 import { Hero } from '../components-v2/sections/Hero';
+import { CallToAction } from '../components-v2/molecules/CallToAction';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <Hero />
+      <Hero className="mb-[116px]" />
+      <CallToAction className="mb-[116px]" />
     </>
   );
 };
