d1aef6d72743a69532151c183cb8f1e638ccbbb5
Kasun Peiris <peiris@kasun.io>
2022-02-05 01:16:17 +0530

    Homepage updates

--- js/packages/web/src/components-v2/sections/CallToAction/CallToAction.tsx
+++ js/packages/web/src/components-v2/sections/CallToAction/CallToAction.tsx
@@ -14,7 +14,7 @@ export const CallToAction: FC<CallToActionProps> = ({
   return (
     <div className={CallToActionClasses} {...restProps}>
       <div className="container">
-        <div className="cursor-pointer flex flex-col rounded-[28px] bg-[linear-gradient(94.38deg,_#448fff_1.06%,_#0066D6_107.04%)] pt-[60px] pb-[60px] items-center justify-center text-center gap-[20px] relative">
+        <div className="cursor-pointer flex flex-col rounded-[28px] bg-[linear-gradient(94.38deg,_#448fff_1.06%,_#005cc1_107.04%)] pt-[60px] pb-[60px] items-center justify-center text-center gap-[20px] relative">
           <span className='absolute top-0 bottom-0 left-0 right-0 bg-[url("/img/branding-bg-1.png")] bg-cover bg-no-repeat' />
           <h2 className="relative z-10 text-white text-h2">
             Launch Your Project
--- js/packages/web/src/components-v2/sections/Hero/Hero.tsx
+++ js/packages/web/src/components-v2/sections/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import CN from 'classnames';
+import { Parallax } from 'react-parallax';
 import { heroSlider } from '../../../../dummy-data/hero-slider';
 import { CardPreview } from '../../molecules/CardPreview';
 
@@ -8,45 +9,53 @@ export interface HeroProps {
 }
 
 export const Hero: FC<HeroProps> = ({ className, ...restProps }: HeroProps) => {
-  const HeroClasses = CN(
-    `hero bg-[url("/img/hero-bg.png")] py-[148px] bg-cover bg-center mt-[85px]`,
-    className,
-  );
+  const HeroClasses = CN(`hero relative mt-[85px]`, className);
 
   return (
     <div className={HeroClasses} {...restProps}>
-      <div className="container flex items-center justify-between">
-        <div className="flex flex-col text-white">
-          <h1 className="text-white text-h1 font-600 w-full max-w-[472px] mb-[24px]">
-            Buy, sell, trade <br />
-            eco-friendly NFTs
-          </h1>
+      <Parallax
+        blur={0}
+        bgImage={'/img/hero-bg.png'}
+        bgImageAlt="Karmaverse"
+        strength={200}
+        bgClassName="!object-cover !h-[900px] !w-full"
+      >
+        <div className="container flex items-center justify-between py-[148px]">
+          <div className="flex flex-col text-white">
+            <h1 className="text-white text-h1 font-600 w-full max-w-[472px] mb-[24px]">
+              Buy, sell, trade <br />
+              eco-friendly NFTs
+            </h1>
 
-          <p className="text-lg w-full max-w-[472px] mb-[20px]">
-            Karmaverse is an eco-friendly NFT exchange that rewards users for
-            their activity and donates a portion of every trade to charitable
-            causes.
-          </p>
+            <p className="text-lg w-full max-w-[472px] mb-[20px]">
+              Karmaverse is an eco-friendly NFT exchange that rewards users for
+              their activity and donates a portion of every trade to charitable
+              causes.
+            </p>
 
-          <a href="#" className="text-G-100 hover:text-G-200 mb-[40px] gap-[4px] flex items-center">
-            Learn more <i className="ri-arrow-right-s-line" />
-          </a>
+            <a
+              href="#"
+              className="text-G-100 hover:text-G-200 mb-[40px] gap-[4px] flex items-center"
+            >
+              Learn more <i className="ri-arrow-right-s-line" />
+            </a>
 
-          <div className="flex items-center gap-[28px] w-full max-w-[472px]">
-            <button className="h-[48px] bg-B-400 hover:bg-B-500 rounded-full px-[20px] w-full uppercase text-md font-500">
-              Browse
-            </button>
+            <div className="flex items-center gap-[28px] w-full max-w-[472px]">
+              <button className="h-[48px] bg-B-400 hover:bg-B-500 rounded-full px-[20px] w-full uppercase text-md font-500">
+                Browse
+              </button>
 
-            <button className="h-[48px] bg-P-400 hover:bg-P-500 rounded-full px-[20px] w-full uppercase text-md font-500">
-              Donate
-            </button>
+              <button className="h-[48px] bg-P-400 hover:bg-P-500 rounded-full px-[20px] w-full uppercase text-md font-500">
+                Donate
+              </button>
+            </div>
+          </div>
+
+          <div className="flex">
+            <CardPreview list={heroSlider} />
           </div>
         </div>
-
-        <div className="flex">
-          <CardPreview list={heroSlider} />
-        </div>
-      </div>
+      </Parallax>
     </div>
   );
 };
