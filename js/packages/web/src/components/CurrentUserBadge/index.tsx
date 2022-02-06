fc2239aae21d8532a7ec7ebf8e7402326dd74ab3
Steven Luscher <steveluscher@users.noreply.github.com>
2022-02-05 17:57:45 -0800

    fix: Prevent number of hooks from changing in user badge component (#1075)
    Co-authored-by: steveluscher <github@steveluscher.com>

--- js/packages/web/src/components/CurrentUserBadge/index.tsx
+++ js/packages/web/src/components/CurrentUserBadge/index.tsx
@@ -209,13 +209,14 @@ export const CurrentUserBadge = (props: {
   const { account } = useNativeAccount();
   const solPrice = useSolPrice();
   const [showAddFundsModal, setShowAddFundsModal] = useState<Boolean>(false);
+  const tokenList = useTokenList();
 
   if (!wallet || !publicKey) {
     return null;
   }
   const balance = (account?.lamports || 0) / LAMPORTS_PER_SOL;
   const balanceInUSD = balance * solPrice;
-  const solMintInfo = useTokenList().tokenMap.get(WRAPPED_SOL_MINT.toString());
+  const solMintInfo = tokenList.tokenMap.get(WRAPPED_SOL_MINT.toString());
   const iconStyle: React.CSSProperties = {
     display: 'flex',
     width: props.iconSize,
