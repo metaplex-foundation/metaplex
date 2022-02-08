ca140bae27abc5cf1407ac4104f9f2bbf67cb4be
Kristian Elset Bø <kristian.e.boe@gmail.com>
2022-02-08 15:22:27 -0600

    fix: node version to 16.14.0 (LTS)

--- js/packages/web/next.config.js
+++ js/packages/web/next.config.js
@@ -28,7 +28,7 @@ module.exports = withPlugins(plugins, {
   },
   productionBrowserSourceMaps: true,
   env: {
-    NEXT_PUBLIC_BUGSNAG_API_KEY: process.env.BUGSNAG_API_KEY,
+    NEXT_PUBLIC_BUGSNAG_API_KEY: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
     NEXT_PUBLIC_ARWEAVE_CDN:
       process.env.ARWEAVE_CDN || 'https://arweave.cache.holaplex.dev',
     NEXT_PUBLIC_STORE_OWNER_ADDRESS:
