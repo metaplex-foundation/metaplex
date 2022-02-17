a2c4f114404ad6a8fc33f10efa3048893695f0d8
Kristian Elset Bø <kristian.e.boe@gmail.com>
2022-02-17 10:02:49 +0100

    fix: restructured css

--- js/packages/web/src/pages/404.tsx
+++ js/packages/web/src/pages/404.tsx
@@ -12,6 +12,25 @@ const NotFound = () => (
     <p>
       Claim this storefront on <a href="https://holaplex.com">Holaplex</a>
     </p>
+    <style jsx>
+      {`
+        @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;800&display=swap');
+
+        #metaplex-not-found {
+          margin-top: 15vh;
+          font-size: 20px;
+
+          --ant-primary-color: #f03e92;
+          --ant-primary-color-hover: lighten(#f03e92, 10);
+          --ant-primary-color-active: darken(#f03e92, 10);
+        }
+
+        h1 {
+          font-family: 'Nunito Sans', Verdana, Geneva, Tahoma, sans-serif;
+          font-weight: 800;
+        }
+      `}
+    </style>
   </Space>
 );
 
