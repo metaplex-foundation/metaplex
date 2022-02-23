13aa68e2e2905983c666ea360e18146c57ccf11f
Juan <juandgoc@gmail.com>
2022-02-22 19:50:05 -0500

    lint

--- js/packages/web/src/components/CollectionCard/index.tsx
+++ js/packages/web/src/components/CollectionCard/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { ArtContent } from "../ArtContent";
-import { shortenAddress, StringPublicKey } from "@oyster/common";
-import { useArt, useExtendedArt } from "../../hooks";
-import { MetaAvatar } from "../MetaAvatar";
+import { ArtContent } from '../ArtContent';
+import { shortenAddress, StringPublicKey } from '@oyster/common';
+import { useArt, useExtendedArt } from '../../hooks';
+import { MetaAvatar } from '../MetaAvatar';
 
-export const CollectionCard = (props: {
-  pubkey: StringPublicKey
-}) => {
+export const CollectionCard = (props: { pubkey: StringPublicKey }) => {
   const { pubkey } = props;
 
   const art = useArt(pubkey);
-  const {ref, data} = useExtendedArt(pubkey);
+  const { ref, data } = useExtendedArt(pubkey);
 
   return (
-    <div className={"collection-card-container"} ref={ref}>
-      <div className={"asset-container"}>
+    <div className={'collection-card-container'} ref={ref}>
+      <div className={'asset-container'}>
         <div className={'art-content-wrapper'}>
           <ArtContent
             className="auction-image no-events"
@@ -24,16 +22,14 @@ export const CollectionCard = (props: {
           />
         </div>
       </div>
-      <div className={"collection-title"}>
-        {art.title}
-      </div>
-      <div className={"collection-description"}>
-        {data?.description}
-      </div>
-      <div className={"collection-artists"}>
+      <div className={'collection-title'}>{art.title}</div>
+      <div className={'collection-description'}>{data?.description}</div>
+      <div className={'collection-artists'}>
         <MetaAvatar creators={art.creators} />
         <span className={'artist-name'}>
-          {art.creators?.map(c => c.name || shortenAddress(c.address ?? '')).join(", ")}
+          {art.creators
+            ?.map(c => c.name || shortenAddress(c.address ?? ''))
+            .join(', ')}
         </span>
       </div>
     </div>
