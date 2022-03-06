e978528e5d174274e3911617fbc1fd0d351b68f0
chathuranga <chathuranga@freshworks.io>
2022-03-06 13:53:28 +0530

    changes

--- js/packages/web/src/views/artworks/hooks/useItems.ts
+++ js/packages/web/src/views/artworks/hooks/useItems.ts
@@ -1,22 +1,25 @@
-import { useMeta } from '@oyster/common'
+import { useMeta, useWallet} from '@oyster/common'
+import { useCreatorArts } from '../../../hooks'
 
 import { ArtworkViewState, Item } from '../types'
+import { usePacksBasedOnProvingProcesses } from './usePacksBasedOnProvingProcesses'
+import { useUserMetadataWithPacks } from './useUserMetadataWithPacks'
 
 export const useItems = ({ activeKey }: { activeKey: ArtworkViewState }): Item[] => {
-  // const { publicKey } = useWallet();
+  const { publicKey } = useWallet();
   console.log(activeKey)
   const { metadata } = useMeta()
-  // const createdMetadata = useCreatorArts(publicKey?.toBase58() || '');
-  // const userMetadataWithPacks = useUserMetadataWithPacks();
-  // const packsBasedOnProvingProcesses = usePacksBasedOnProvingProcesses();
+  const createdMetadata = useCreatorArts(publicKey?.toBase58() || '');
+  const userMetadataWithPacks = useUserMetadataWithPacks();
+  const packsBasedOnProvingProcesses = usePacksBasedOnProvingProcesses();
 
-  // if (activeKey === ArtworkViewState.Owned) {
-  //   return [...userMetadataWithPacks, ...packsBasedOnProvingProcesses];
-  // }
+  if (activeKey === ArtworkViewState.Owned) {
+    return [...userMetadataWithPacks, ...packsBasedOnProvingProcesses];
+  }
 
-  // if (activeKey === ArtworkViewState.Created) {
-  //   return createdMetadata;
-  // }
+  if (activeKey === ArtworkViewState.Created) {
+    return createdMetadata;
+  }
 
   return metadata
 }
