6583ea92d488f20b61c4f52698498d069b9ffb7c
chathuranga <chathuranga@freshworks.io>
2022-03-07 13:18:57 +0530

    added fixes

--- js/packages/web/src/views/artworks/hooks/useItems.ts
+++ js/packages/web/src/views/artworks/hooks/useItems.ts
@@ -1,24 +1,25 @@
-import { useMeta, useWallet} from '@oyster/common'
-import { useCreatorArts } from '../../../hooks'
+import { useMeta } from '@oyster/common'
+import { useWallet } from '@solana/wallet-adapter-react'
 
+import { useCreatorArts } from '../../../hooks'
 import { ArtworkViewState, Item } from '../types'
-import { usePacksBasedOnProvingProcesses } from './usePacksBasedOnProvingProcesses'
+
 import { useUserMetadataWithPacks } from './useUserMetadataWithPacks'
+import { usePacksBasedOnProvingProcesses } from './usePacksBasedOnProvingProcesses'
 
 export const useItems = ({ activeKey }: { activeKey: ArtworkViewState }): Item[] => {
-  const { publicKey } = useWallet();
-  console.log(activeKey)
+  const { publicKey } = useWallet()
   const { metadata } = useMeta()
-  const createdMetadata = useCreatorArts(publicKey?.toBase58() || '');
-  const userMetadataWithPacks = useUserMetadataWithPacks();
-  const packsBasedOnProvingProcesses = usePacksBasedOnProvingProcesses();
+  const createdMetadata = useCreatorArts(publicKey?.toBase58() || '')
+  const userMetadataWithPacks = useUserMetadataWithPacks()
+  const packsBasedOnProvingProcesses = usePacksBasedOnProvingProcesses()
 
   if (activeKey === ArtworkViewState.Owned) {
-    return [...userMetadataWithPacks, ...packsBasedOnProvingProcesses];
+    return [...userMetadataWithPacks, ...packsBasedOnProvingProcesses]
   }
 
   if (activeKey === ArtworkViewState.Created) {
-    return createdMetadata;
+    return createdMetadata
   }
 
   return metadata
