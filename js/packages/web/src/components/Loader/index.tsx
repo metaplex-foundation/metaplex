3a54cbfc03637847430d0d3b336c2ba159231924
Kasun Peiris <peiris@kasun.io>
2022-03-13 02:26:20 +0530

    Slight cleanup

--- js/packages/web/src/components/Loader/index.tsx
+++ js/packages/web/src/components/Loader/index.tsx
@@ -20,19 +20,3 @@ export const LoaderProvider: FC = ({ children }) => {
     </>
   )
 }
-
-export const Spinner = () => {
-  return (
-    <div className='spinner'>
-      <span className='line line-1' />
-      <span className='line line-2' />
-      <span className='line line-3' />
-      <span className='line line-4' />
-      <span className='line line-5' />
-      <span className='line line-6' />
-      <span className='line line-7' />
-      <span className='line line-8' />
-      <span className='line line-9' />
-    </div>
-  )
-}
