6be3ad09a7b9acebfb53e1255837577375291952
rascrypt <75642818+vinceeverton@users.noreply.github.com>
2022-03-14 08:23:49 -0400

    Revert "Update index.tsx"
    This reverts commit 508729a05ffe2718f49703af35f9aaeb45b0f06c.

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -53,7 +53,7 @@ export const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/sol.svg'} />}
+          title={<img src={'/wQlgQW01.svg'} />}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -112,7 +112,7 @@ export const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/sol.svg'} />
+      <img src={'/wQlgQW01.svg'} />
     </Link>
   );
 };
