c1b7147ab2168606cdb46d6bac536bb6bdd7737e
Abhi <enigmaticabhisek@gmail.com>
2022-03-23 17:52:46 +0530

    Added creator profile icons, storefront accordion, fixed twitter button, added separator

--- js/packages/web/src/components/Banner/index.tsx
+++ js/packages/web/src/components/Banner/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Typography, Button, Alert } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Typography, Button, Alert, Space } from 'antd';
 import { TwitterOutlined } from '@ant-design/icons';
-import { StorefrontSocialInfo } from '@oyster/common';
-import { useWallet } from '@oyster/common';
+import { Connection, StorefrontSocialInfo } from '@oyster/common';
+import { useWallet, useMeta } from '@oyster/common';
 const { Text } = Typography;
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
+import { Identicon, shortenAddress, getTwitterHandle, useConnection } from '@oyster/common';
+import { ChevronRightIcon } from '@heroicons/react/solid';
 
 export const Banner = ({
   src,
@@ -25,6 +28,20 @@ export const Banner = ({
   ownerAddress?: string;
 }) => {
   const wallet = useWallet();
+  const { whitelistedCreatorsByCreator } = useMeta();
+  const creators = Object.values(whitelistedCreatorsByCreator);
+
+  const connection = useConnection();
+
+  const [active, setActive] = useState(false);
+  const [height, setHeight] = useState('0px');
+  const contentSpace = useRef(null);
+
+  function toggleAccordion() {
+    setActive(active === false ? true : false);
+    //@ts-ignore
+    setHeight(active ? '0px' : `${contentSpace.current.scrollHeight}px`);
+  }
 
   return (
     <div id="metaplex-banner">
@@ -33,55 +50,129 @@ export const Banner = ({
       ) : (
         <div className="metaplex-margin-top-12"></div>
       )}
-
       <div className="logo-wrapper">
         <Link to="/" id="metaplex-header-logo">
           <img src={logo || ''} />
         </Link>
       </div>
-
       <div id="metaplex-banner-hero">
         <h1>{headingText}</h1>
         {subHeadingText && <Text>{subHeadingText}</Text>}
-        {twitterVerification ? (
-          <a
-            href={'https://twitter.com/' + twitterVerification}
-            target="_blank"
-            rel="noreferrer"
-            className="twitter-button"
-          >
-            {' '}
-            <Button shape="round" icon={<TwitterOutlined />}>
-              @{twitterVerification}
-            </Button>
-          </a>
-        ) : (
-          wallet.connected &&
-          ownerAddress === wallet.publicKey?.toBase58() && (
-            <div className="metaplex-margin-top-8">
-              <Alert
-                className="metaplex-flex-align-items-center metaplex-align-left"
-                message="Connect your Twitter account"
-                description={
-                  <>
-                    Help protect collectors by connecting your store to a Twitter page on{' '}
-                    <a
-                      href="https://naming.bonfida.org/#/twitter-registration"
-                      rel="noreferrer"
-                      target="_blank"
-                    >
-                      Bonfida
-                    </a>
-                  </>
-                }
-                icon={<TwitterOutlined />}
-                showIcon
-              />
+        <div className="flex justify-center pt-4">
+          <div className="pr-4">
+            {twitterVerification ? (
+              <a
+                href={'https://twitter.com/' + twitterVerification}
+                target="_blank"
+                rel="noreferrer"
+                className="twitter-button"
+              >
+                {' '}
+                <Space>
+                  <Button
+                    type="text"
+                    block
+                    shape="round"
+                    icon={
+                      <TwitterOutlined style={{ fontSize: '18px', verticalAlign: 'text-top' }} />
+                    }
+                  >
+                    @{twitterVerification}
+                  </Button>
+                  <div className=''>
+                    <h3 className='text-primary'>|</h3>
+                  </div>
+                </Space>
+              </a>
+            ) : (
+              wallet.connected &&
+              ownerAddress === wallet.publicKey?.toBase58() && (
+                <div className="metaplex-margin-top-8">
+                  <Alert
+                    className="metaplex-flex-align-items-center metaplex-align-left"
+                    message="Connect your Twitter account"
+                    description={
+                      <>
+                        Help protect collectors by connecting your store to a Twitter page on{' '}
+                        <a
+                          href="https://naming.bonfida.org/#/twitter-registration"
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          Bonfida
+                        </a>
+                      </>
+                    }
+                    icon={<TwitterOutlined />}
+                    showIcon
+                  />
+                </div>
+              )
+            )}
+          </div>
+          <div className="flex cursor-pointer pt-1" onClick={toggleAccordion}>
+            Created by
+            <div className="flex -space-x-2 overflow-hidden pl-2">
+              {creators.map((m) => {
+                const current = m.info.address;
+                return (
+                  <a href={`https://www.holaplex.com/profiles/${current}`}>
+                    <Identicon size={22} address={current} />
+                  </a>
+                );
+              })}
             </div>
-          )
-        )}
+          </div>
+        </div>
       </div>
       {children}
+
+      <div className="px-8">
+        <div
+          ref={contentSpace}
+          style={{ maxHeight: `${height}` }}
+          className="transition-max-height overflow-auto duration-700 ease-in-out"
+        >
+          <div className="pb-10">
+            <div className="w-screen flex-row px-12">
+              {creators.map((m) => {
+                const current = m.info.address;
+                const [creatorTwitterHandle, setCreatorTwitterHandle] = useState('');
+                useEffect(() => {
+                  getTwitterHandle(connection, current).then(
+                    (tw) => tw && setCreatorTwitterHandle(tw)
+                  );
+                }, []);
+
+                return (
+                  <div className="mb-3 flex-row">
+                    <a href={`https://www.holaplex.com/profiles/${current}`}>
+                      <div  className=" relative flex rounded-md border p-3 pl-4 border-white/50">
+                        <Identicon size={58} address={current} />
+
+                        <div className=" pt-4 pl-4">
+                          {creatorTwitterHandle
+                            ? `@${creatorTwitterHandle}`
+                            : shortenAddress(current)}
+                        </div>
+
+                        <div className="absolute inset-y-0 right-4 flex items-center !opacity-100">
+                          View Profile
+                          <ChevronRightIcon
+                            className="h-5 w-5"
+                            viewBox="0 0 18 18"
+                            aria-hidden="true"
+                          />
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
