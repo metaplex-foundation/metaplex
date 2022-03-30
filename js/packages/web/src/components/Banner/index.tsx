74e5a912e144145c81e3f324421b7b4fa8c519dc
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-03-30 19:46:37 +0200

    fix: removed popover for now

--- js/packages/web/src/components/Banner/index.tsx
+++ js/packages/web/src/components/Banner/index.tsx
@@ -9,13 +9,13 @@ import { Link } from 'react-router-dom';
 import { Identicon, shortenAddress, getTwitterHandle, useConnection } from '@oyster/common';
 
 import { Popover, Transition, Dialog } from '@headlessui/react';
-
+import { CheckIcon, DuplicateIcon, XIcon } from '@heroicons/react/outline';
+import classNames from 'classnames';
 
 export const Banner = ({
   src,
   headingText,
   subHeadingText,
-  children,
   logo,
   twitterVerification,
   ownerAddress,
@@ -23,7 +23,6 @@ export const Banner = ({
   src?: string;
   headingText: string;
   subHeadingText?: string;
-  children?: React.ReactNode;
   logo: string;
   twitterVerification?: string;
   social?: StorefrontSocialInfo;
@@ -34,112 +33,134 @@ export const Banner = ({
   const creators = Object.values(whitelistedCreatorsByCreator);
 
   const connection = useConnection();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   // const [isShowing, setIsShowing] = useState(false)
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
 
+  const FollowButton = () => (
+    <button className="flex font-sans  items-center justify-center transition-all rounded-full !bg-white !px-6 !py-2 !text-base !font-medium  !text-gray-900 hover:!text-gray-700 hover:!bg-gray-100">
+      Follow
+    </button>
+  );
 
   const CreatedByAvatars = ({ className }: { className?: string }) => (
+    <div onClick={openModal} className={'flex cursor-pointer items-center ' + className}>
+      <div> Created by </div>
 
-
-    <div className={'flex cursor-pointer items-center ' + className}>
-      
-     <div onClick={openModal}> Created by </div>
-     
-      
       <div className="flex -space-x-2 overflow-hidden pl-2">
         {creators.map((m) => {
           const creatorAddress = m.info.address;
           const [creatorTwitterHandle, setCreatorTwitterHandle] = useState('');
-                  useEffect(() => {
-                    getTwitterHandle(connection, creatorAddress).then(
-                      (tw) => tw && setCreatorTwitterHandle(tw)
-                    );
-                  }, []);
+          useEffect(() => {
+            getTwitterHandle(connection, creatorAddress).then(
+              (tw) => tw && setCreatorTwitterHandle(tw)
+            );
+          }, []);
+
+          const [pOpen, setPOpen] = useState(false);
+
+          const [copied, setCopeied] = useState(false);
+          const copyPubKey = async () => {
+            if (creatorAddress) {
+              await navigator.clipboard.writeText(creatorAddress);
+              setCopeied(true);
+              setTimeout(() => setCopeied(false), 2000);
+            }
+          };
 
           return (
-            
             // <a
             //   href={`https://www.holaplex.com/profiles/${creatorAddress}`}
             //   key={creatorAddress}
             //   target="_blank"
             //   rel="noreferrer"
-            // > 
-            <div key={creatorAddress}>
-            <Popover >
+            // >
+            <Popover key={creatorAddress}>
+              {({ open }) => (
+                <div>
+                  <Popover.Button
+                    onClick={() => setPOpen(!pOpen)}
+                    onMouseEnter={() => setPOpen(true)}
+                    className={classNames('flex items-center', pOpen && 'z-50')}
 
-                <Popover.Button as='div' 
-               // onMouseEnter={() => setIsShowing(true)}
-               // onMouseLeave={() => setIsShowing(false)}
-                >
-                <Identicon size={22} address={creatorAddress} />
-                </Popover.Button>
+                    // onMouseLeave={() => setPOpen(false)}
+                  >
+                    <Identicon size={22} address={creatorAddress} />
+                  </Popover.Button>
 
-                <Transition key={creatorAddress}
-              // show={isShowing}
-             //  onMouseEnter={() => setIsShowing(true)}
-             //  onMouseLeave={() => setIsShowing(false)}
-             enter="transition duration-300 ease-out"
-             enterFrom="transform scale-0 opacity-0"
-             enterTo="transform scale-100 opacity-100"
-             leave="transition duration-300 ease-out"
-             leaveFrom="transform scale-100 opacity-100"
-             leaveTo="transform scale-0 opacity-0"
-            >
-
-            
-
-                <Popover.Panel as='div'
-                
-                 className=" translate-x-3 absolute overflow-y-auto " >
-                <div className="mb-3 flex-row w-full" key={creatorAddress}>
-                      <a
-                        href={`https://www.holaplex.com/profiles/${creatorAddress}`}
-                        target="_blank"
-                        rel="noreferrer"
+                  <Transition
+                    enter="transition duration-300 ease-out"
+                    enterFrom="transform scale-0 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-300 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-0 opacity-0"
+                  >
+                    {false && (
+                      <Popover.Panel
+                        static
+                        className=" translate-x-3 absolute overflow-y-auto z-50 max-w-xs w-full  "
                       >
-                        <div className=" relative flex-col rounded-md border p-3 pl-4 border-white/50 !bg-black w-full">
-                          
-                          <div className='flex space-x-20'>
-                          <Identicon size={58} address={creatorAddress} />
+                        <div className="mb-3 flex-row w-full">
+                          <div className=" relative flex-col rounded-md border p-3 pl-4 border-white/50 bg-gray-900 w-full">
+                            <div className="flex justify-between items-center">
+                              <Identicon size={40} address={creatorAddress} />
 
-                          <Button className="!rounded-full !text-color-text-accent !font-theme-title !bg-white mt-3 right-4 sm:flex items-center ml-auto ">
-                            Follow
-                          </Button>
-
+                              <a
+                                href={`https://www.holaplex.com/profiles/${creatorAddress}?action=follow`}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                <FollowButton />
+                              </a>
+                            </div>
+                            <div className="mt-4 flex items-center">
+                              <a
+                                href={`https://www.holaplex.com/profiles/${creatorAddress}`}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                <div
+                                  className={classNames(
+                                    'text-2xl',
+                                    creatorTwitterHandle ? 'font-sans' : 'font-mono'
+                                  )}
+                                >
+                                  {creatorTwitterHandle
+                                    ? `@${creatorTwitterHandle}`
+                                    : shortenAddress(creatorAddress)}
+                                </div>
+                              </a>
+                              {copied ? (
+                                <CheckIcon className="ml-4 h-7 w-7  hover:text-gray-300" />
+                              ) : (
+                                <DuplicateIcon
+                                  className="ml-4 h-7 w-7 cursor-pointer  hover:text-gray-300"
+                                  onClick={copyPubKey}
+                                />
+                              )}
+                            </div>
                           </div>
-                          <div className="mt-4">
-                            {creatorTwitterHandle
-                              ? `@${creatorTwitterHandle}`
-                              : shortenAddress(creatorAddress)}
-                          </div>
-
                         </div>
-                      </a>
-                    </div>
-            
-                </Popover.Panel>
-
-                </Transition>
-                </Popover>
-             </div>
-            
+                      </Popover.Panel>
+                    )}
+                  </Transition>
+                </div>
+              )}
+            </Popover>
           );
-          
-
         })}
       </div>
     </div>
   );
 
   return (
-    
     <div>
       <div id="metaplex-banner">
         {src ? (
@@ -214,93 +235,100 @@ export const Banner = ({
             </div>
           )}
         </div>
-        {children}
-        <div>
-         <Transition
-          appear show={isOpen} as={Fragment}
-        > 
-        <Dialog as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          open={isOpen} onClose={closeModal}>
-          
-          <div className="min-h-screen px-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
 
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-10 overflow-y-auto"
+            open={isOpen}
+            onClose={closeModal}
+          >
+            <div className="min-h-screen px-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0" />
+              </Transition.Child>
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span className="inline-block h-screen align-middle" aria-hidden="true">
+                &#8203;
+              </span>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <div className="inline-block w-full max-w-md relative pt-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-gray-900 shadow-xl rounded-2xl ">
+                  <XIcon
+                    className="text-white h-6 w-6 absolute top-4 right-4 cursor-pointer hover:text-gray-100"
+                    onClick={closeModal}
+                    aria-hidden="true"
+                  />
+                  <Dialog.Title
+                    as="h3"
+                    className="text-center pb-4 border-b-2  font-medium text-white"
+                  >
+                    Creators
+                  </Dialog.Title>
+                  <div
+                    className={classNames(
+                      'space-y-6 h-96 p-6 ',
+                      // could be some Windows funk, but without this the scrollbar was always present
+                      creators.length > 6 && 'overflow-y-scroll'
+                    )}
+                  >
+                    {creators.map((m) => {
+                      const creatorAddress = m.info.address;
+                      const [creatorTwitterHandle, setCreatorTwitterHandle] = useState('');
+                      useEffect(() => {
+                        getTwitterHandle(connection, creatorAddress).then(
+                          (tw) => tw && setCreatorTwitterHandle(tw)
+                        );
+                      }, []);
 
-          <Dialog.Overlay className="fixed inset-0" />
-          </Transition.Child>
-          <span
-              className="inline-block h-screen align-middle"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-black shadow-xl rounded-2xl">
-            
-            <Dialog.Title className="text-center pb-2 border-b-2">Creators</Dialog.Title>
-              <Dialog.Description className="flex-col  ">
-                {creators.map((m) => {
-                  const creatorAddress = m.info.address;
-                  const [creatorTwitterHandle, setCreatorTwitterHandle] = useState('');
-                  useEffect(() => {
-                    getTwitterHandle(connection, creatorAddress).then(
-                      (tw) => tw && setCreatorTwitterHandle(tw)
-                    );
-                  }, []);
+                      return (
+                        <a
+                          key={creatorAddress}
+                          href={`https://www.holaplex.com/profiles/${creatorAddress}`}
+                          target="_blank"
+                          rel="noreferrer"
+                          className=" flex w-full items-center"
+                        >
+                          <Identicon size={40} address={creatorAddress} />
 
-                  return (
-                    <div className="mb-3 flex-row w-full" key={creatorAddress}>
-                      <a
-                        href={`https://www.holaplex.com/profiles/${creatorAddress}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <div className=" relative flex rounded-md p-3 pl-4 border-white/50 w-full">
-                          <Identicon size={58} address={creatorAddress} />
-
-                          <div className=" pt-4 pl-4">
+                          <div
+                            className={classNames(
+                              'ml-3 mr-auto',
+                              creatorTwitterHandle ? 'font-sans' : 'font-mono'
+                            )}
+                          >
                             {creatorTwitterHandle
                               ? `@${creatorTwitterHandle}`
                               : shortenAddress(creatorAddress)}
                           </div>
 
-                          <Button className="!rounded-full !text-color-text-accent !font-theme-title !bg-white mt-3 right-4 sm:flex items-center ml-auto ">
-                            Follow
-                          </Button>
-                        </div>
-                      </a>
-                    </div>
-                  );
-                })}
-              </Dialog.Description>
+                          {/* <Button className="!rounded-full !text-color-text-accent !font-theme-title !bg-white mt-3 right-4 sm:flex items-center ml-auto "> */}
+                          <FollowButton />
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Transition.Child>
             </div>
-
-            {/* </div> */}
-          </Transition.Child>
-          
-          </div>
           </Dialog>
         </Transition>
-        </div>
-        </div>
+      </div>
     </div>
-    
   );
 };
