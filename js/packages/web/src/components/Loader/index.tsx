659e1d6bffb0ffab6e3a8342f126ef63d66d5ae9
Kasun Peiris <peiris@kasun.io>
2022-03-31 18:28:39 +0530

    Header styles update

--- js/packages/web/src/components/Loader/index.tsx
+++ js/packages/web/src/components/Loader/index.tsx
@@ -8,7 +8,7 @@ export const LoaderProvider: FC = ({ children }) => {
     <>
       {isLoading && (
         <div
-          className={`loader-container fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-white`}>
+          className={`loader-container fixed top-0 left-0 right-0 bottom-0 z-[1000] flex items-center justify-center bg-white`}>
           <div className='loader-block flex flex-col justify-center gap-[20px]'>
             <Loader />
             <h3>Processing your experience</h3>
