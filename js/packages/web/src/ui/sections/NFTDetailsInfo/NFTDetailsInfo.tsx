844397ea01c9ff3cf0ae340ded6ce3d8760d116d
rusiruavb <rusiruavb98@gmail.com>
2022-04-07 14:08:54 +0530

    Add NFT details under `Details` tab

--- js/packages/web/src/ui/sections/NFTDetailsInfo/NFTDetailsInfo.tsx
+++ js/packages/web/src/ui/sections/NFTDetailsInfo/NFTDetailsInfo.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import CN from 'classnames'
-import { AuctionView } from '../../../hooks'
+import { Tooltip } from 'antd'
+import { AuctionView, useArt } from '../../../hooks'
+import { useState } from 'react'
+import { CopyOutlined } from '@ant-design/icons'
 
 export interface NFTDetailsInfoProps {
   auction: AuctionView
@@ -8,34 +11,80 @@ export interface NFTDetailsInfoProps {
 
 export const NFTDetailsInfo: FC<NFTDetailsInfoProps> = ({ auction }) => {
   const NFTDetailsInfoClasses = CN(`nft-details-info w-full`)
-  // console.log('auction', auction)
+  const art = useArt(auction.thumbnail.metadata.pubkey)
+  const [mint, setMint] = useState<string>()
+  const [tokenAddress, setTokenAddress] = useState<string>()
+  const [owner, setOwner] = useState<any>()
+  const [royalties, setRoyalties] = useState<number>()
+  const [transactionFee, setTransactionFee] = useState<string>()
+  
+  useEffect(() => {
+    if (
+      auction.thumbnail && 
+      auction.thumbnail.metadata && 
+      auction.thumbnail.metadata.account &&
+      auction.thumbnail.metadata.info && 
+      auction.auction && 
+      auction.auction.info
+    ) {
+      setMint(auction.thumbnail.metadata.info.mint)
+      setTokenAddress(auction.auction.info.tokenMint)
+      setOwner(auction.thumbnail.metadata.account.owner)
+      setRoyalties(art.seller_fee_basis_points)
+      setTransactionFee('2')
+    }
+  }, [])
 
   return (
     <div className={NFTDetailsInfoClasses}>
       <div className='grid w-full grid-cols-1 gap-[4px]'>
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
           <label className='font-500'>Mint address</label>
-          <span>6gr...Vun5</span>
+          <Tooltip title='Address copied'>
+              <div
+                className='flex items-center gap-[4px] font-500'
+                onClick={() => navigator.clipboard.writeText(mint || '')}
+              >
+                <CopyOutlined />
+                {`${mint?.substring(0,3)}...${mint?.substring(mint.length - 4)}`}
+              </div>
+            </Tooltip>
         </div>
 
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
           <label className='font-500'>Token address</label>
-          <span>6gr...Vun5</span>
+          <Tooltip title='Address copied'>
+              <div
+                className='flex items-center gap-[4px] font-500'
+                onClick={() => navigator.clipboard.writeText(tokenAddress || '')}
+              >
+                <CopyOutlined />
+                {`${tokenAddress?.substring(0,3)}...${tokenAddress?.substring(tokenAddress.length -4)}`}
+              </div>
+            </Tooltip>
         </div>
 
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
           <label className='font-500'>Owner</label>
-          <span>6gr...Vun5</span>
+          <Tooltip title='Address copied'>
+              <div
+                className='flex items-center gap-[4px] font-500'
+                onClick={() => navigator.clipboard.writeText(owner || '')}
+              >
+                <CopyOutlined />
+                {`${owner?.substring(0,3)}...${owner?.substring(owner.length -4)}`}
+              </div>
+            </Tooltip>
         </div>
 
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
           <label className='font-500'>Artist royalties</label>
-          <span>12.5%</span>
+          <span>{((royalties || 0) / 100).toFixed(2)}%</span>
         </div>
 
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
           <label className='font-500'>Transaction fee</label>
-          <span>2%</span>
+          <span>{transactionFee}%</span>
         </div>
 
         <div className='grid w-full grid-cols-2 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md text-slate-800 shadow-card'>
