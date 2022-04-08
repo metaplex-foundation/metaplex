70f67d4af083e0bdc55defbe6038d65d43cf295d
rusiruavb <rusiruavb98@gmail.com>
2022-04-09 01:22:06 +0530

    Add bid values to current offers tab

--- js/packages/web/src/ui/sections/NFTDetailsCurrentOffers/NFTDetailsCurrentOffers.tsx
+++ js/packages/web/src/ui/sections/NFTDetailsCurrentOffers/NFTDetailsCurrentOffers.tsx
@@ -1,75 +1,27 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import CN from 'classnames'
-import { SOLIcon, Avatar } from '@oyster/common'
+import { SOLIcon, Avatar, formatTokenAmount } from '@oyster/common'
+import { AuctionView, useBidsForAuction } from '../../../hooks'
+import useNFTData from '../../../hooks/useNFTData'
 
 export interface NFTDetailsCurrentOffersProps {
   [x: string]: any
+  auction: AuctionView
 }
-
-const offers = [
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-  {
-    from: '6grFa...Vun5',
-    price: '1.25',
-    floorDifference: '11% below',
-    floorDifferenceType: 'above',
-    expiration: '12 hours',
-  },
-]
 
 export const NFTDetailsCurrentOffers: FC<NFTDetailsCurrentOffersProps> = ({
   className,
   ...restProps
 }: NFTDetailsCurrentOffersProps) => {
   const NFTDetailsCurrentOffersClasses = CN(`nft-details-current-offers w-full`, className)
+  const bids = useBidsForAuction(restProps.auction.auction.pubkey || '')
+  const {
+    remaining: { remainingHours },
+  } = useNFTData(restProps.auction)
+
+  useEffect(() => {
+    console.log('Data: ', bids)
+  }, [])
 
   return (
     <div className={NFTDetailsCurrentOffersClasses} {...restProps}>
@@ -81,28 +33,32 @@ export const NFTDetailsCurrentOffers: FC<NFTDetailsCurrentOffersProps> = ({
       </div>
 
       <div className='flex flex-col gap-[4px]'>
-        {offers.map(
-          (
-            { from, price, floorDifference, floorDifferenceType, expiration }: any,
-            index: number
-          ) => (
-            <div
-              className='grid grid-cols-4 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md font-400 text-slate-800 shadow-card'
-              key={index}>
-              <div className='grid-cell'>
-                <Avatar size='sm' image='' label={from} labelClassName='font-400' />
-              </div>
+        {bids.map((bid, index) => (
+          <div
+            className='grid grid-cols-4 items-center rounded-[8px] border border-slate-200 bg-white p-[8px] text-md font-400 text-slate-800 shadow-card'
+            key={index}>
+            <div className='grid-cell'>
+              <Avatar
+                size='sm'
+                image=''
+                label={`${bid.info.bidderPubkey.substring(
+                  0,
+                  3
+                )}...${bid.info.bidderPubkey.substring(bid.info.bidderPubkey.length - 4)}`}
+                labelClassName='font-400'
+              />
+            </div>
 
-              <div className='grid-cell'>
-                <span className='flex items-center gap-[4px]'>
-                  <SOLIcon size={12} />
-                  <span>{price}</span>
-                </span>
-              </div>
+            <div className='grid-cell'>
+              <span className='flex items-center gap-[4px]'>
+                <SOLIcon size={12} />
+                <span>{formatTokenAmount(bid.info.lastBid)}</span>
+              </span>
+            </div>
 
-              <div className='grid-cell'>
+            {/* <div className='grid-cell'>
                 <span className='flex items-center gap-[4px]'>
-                  <span>{floorDifference}</span>
+                  <span></span>
 
                   {floorDifferenceType === 'above' ? (
                     <i className='ri-arrow-up-line text-[16px] font-400' />
@@ -110,12 +66,14 @@ export const NFTDetailsCurrentOffers: FC<NFTDetailsCurrentOffersProps> = ({
                     <i className='ri-arrow-down-line text-[16px] font-400' />
                   )}
                 </span>
-              </div>
-
-              <div className='grid-cell'>{expiration}</div>
+              </div> */}
+            <div className='grid-cell'>
+              <span className='flex items-center gap-[4px]'></span>
             </div>
-          )
-        )}
+
+            <div className='grid-cell'>{remainingHours}h</div>
+          </div>
+        ))}
       </div>
     </div>
   )
