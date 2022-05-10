6e99ee88944fce10780eb60b5442793f86ebf97c
charithAmila <dkcadissanayaka@gmail.com>
2022-05-11 00:01:44 +0530

    changes

--- js/packages/web/src/hooks/useAuctionPrice.ts
+++ js/packages/web/src/hooks/useAuctionPrice.ts
@@ -1,4 +1,4 @@
-import { fromLamports } from '@oyster/common'
+import { fromLamports, PriceFloorType } from '@oyster/common'
 import { AuctionView } from './useAuctions'
 
 const useAuctionPrice = ({ auctionView }: { auctionView: AuctionView }) => {
@@ -6,12 +6,12 @@ const useAuctionPrice = ({ auctionView }: { auctionView: AuctionView }) => {
   //   const bids = useBidsForAuction(auctionView.auction.pubkey)
   //   const mintInfo = useMint(auctionView.auction.info.tokenMint)
 
-  const participationFixedPrice = auctionView.auctionManager.participationConfig?.fixedPrice || 0
-  const participationOnly = auctionView.auctionManager.numWinners.toNumber() === 0
-  const priceFloor =
-    auctionView.auction.info.priceFloor.type === PriceFloorType.Minimum
-      ? auctionView.auction.info.priceFloor.minPrice?.toNumber() || 0
-      : 0
+  // const participationFixedPrice = auctionView.auctionManager.participationConfig?.fixedPrice || 0
+  // const participationOnly = auctionView.auctionManager.numWinners.toNumber() === 0
+  // const priceFloor =
+  //   auctionView.auction.info.priceFloor.type === PriceFloorType.Minimum
+  //     ? auctionView.auction.info.priceFloor.minPrice?.toNumber() || 0
+  //     : 0
   //   const isUpcoming = auctionView.state === AuctionViewState.Upcoming
   //   const isStarted = auctionView.state === AuctionViewState.Live
 
@@ -21,7 +21,7 @@ const useAuctionPrice = ({ auctionView }: { auctionView: AuctionView }) => {
   //   const ended = isEnded(state)
 
   return {
-    price: fromLamports(participationOnly ? participationFixedPrice : priceFloor, mintInfo),
+    // price: fromLamports(participationOnly ? participationFixedPrice : priceFloor, mintInfo),
   }
 }
 
