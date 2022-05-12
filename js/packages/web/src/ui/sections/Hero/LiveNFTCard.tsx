180609c03b9a3967ac655a7e6c7824abdde9f6c0
charithAmila <dkcadissanayaka@gmail.com>
2022-05-13 00:20:35 +0530

    Display name of the Artist/Collection

--- js/packages/web/src/ui/sections/Hero/LiveNFTCard.tsx
+++ js/packages/web/src/ui/sections/Hero/LiveNFTCard.tsx
@@ -1,8 +1,8 @@
-import { BidCard, Collection, pubkeyToString } from '@oyster/common'
-import { FC } from 'react'
-import { AuctionView, useExtendedArt } from '../../../hooks'
+import { BidCard, pubkeyToString } from '@oyster/common'
+import { FC, useEffect, useState } from 'react'
+import { getProfile } from '../../../api'
+import { useExtendedArt } from '../../../hooks'
 import { CollectionView } from '../../../hooks/useCollections'
-import useNFTData from '../../../hooks/useNFTData'
 import { useAuctionsList } from '../../../views/home/components/SalesList/hooks/useAuctionsList'
 import { LiveAuctionViewState } from '../../views'
 
@@ -18,6 +18,7 @@ const LiveNFTCard: FC<LiveNFTCardProps> = ({ collection, ...rest }) => {
 
   const nameProp: { name: string } = { name: data?.name ?? '' }
   const { auctions } = useAuctionsList(LiveAuctionViewState.All)
+  const [user, setUser] = useState<any>(null)
   //@ts-ignore
   if (data?.collection?.name) {
     //@ts-ignore
@@ -29,19 +30,32 @@ const LiveNFTCard: FC<LiveNFTCardProps> = ({ collection, ...rest }) => {
       return auction.thumbnail.metadata.info.collection?.key === pubkeyToString(collection.mint)
     }) || null
 
+  useEffect(() => {
+    if (!user) {
+      const userPubKey = firstAuction?.auctionManager?.authority || null
+      if (userPubKey) {
+        getProfile(userPubKey).then(({ data }) => {
+          setUser(data)
+        })
+      }
+    }
+  }, [firstAuction, user])
+
   let avatarLabel = ''
-  if (firstAuction?.auctionManager?.authority) {
+  if (firstAuction?.auctionManager?.authority && !user) {
     avatarLabel =
       firstAuction?.auctionManager?.authority.slice(0, 6) +
       '...' +
       firstAuction?.auctionManager?.authority.slice(
         firstAuction?.auctionManager?.authority.length - 6
       )
+  } else if (user) {
+    avatarLabel = user.user_name
   }
   return (
     <BidCard
       {...rest}
-      avatar='https://images.unsplash.com/photo-1511485977113-f34c92461ad9?crop=faces&fit=crop&h=200&w=200'
+      avatar={user?.image ?? ''}
       avatarLabel={avatarLabel}
       image={data?.image ?? ''}
       // remainingTime={`${remainingHours}h : ${remainingMinutes}m : ${remainingSeconds}s`}
