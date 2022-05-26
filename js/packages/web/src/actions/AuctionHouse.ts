61e8bdb84d9c41eb00dcc0bc503a1ce98820901e
chathuranga <chathuranga@freshworks.io>
2022-05-26 23:25:08 +0530

    aded new changes

--- js/packages/web/src/actions/AuctionHouse.ts
+++ js/packages/web/src/actions/AuctionHouse.ts
@@ -19,8 +19,8 @@ export function listAuctionHouseNFT(connection: any, wallet: any): any {
   //   const auctionHouseFee = (listPrice * auctionHouseSellerFee) / 10000;
 
   const getAH = async () => {
-    //const ah = await getAuctionHouse(process.env.NEXT_PUBLIC_STORE_OWNER_ADDRESS as string)
-    let ah = await getAuctionHouse('Da84ovDiz8rVAaLVw8b2JZ7qcP75cBXPTbtLq5ey4Po6')
+    let ah = await getAuctionHouse(process.env.NEXT_PUBLIC_STORE_OWNER_ADDRESS as string)
+    //let ah = await getAuctionHouse('Da84ovDiz8rVAaLVw8b2JZ7qcP75cBXPTbtLq5ey4Po6')
     ah = ah[0]
     debugger
     const auctionHouse: AuctionHouse = {
