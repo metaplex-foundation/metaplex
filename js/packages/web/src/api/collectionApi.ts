76f7c3df5f880de16872cb57dac4d0480a4dbc65
nadunlanka <nadun_chat@yahoo.com>
2022-05-31 11:03:08 +0530

    collection activity and graph connected to API

---
+++ js/packages/web/src/api/collectionApi.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const api = 'http://localhost:9000'
+
+export const getCollectionStatistics = async (collection: string) => {
+  try {
+    const res = await axios.get(`${api}/nft/statistics/${collection}`)
+    return res.data
+  } catch (error: any) {
+    console.log('Get collection statistics API error: ', error.response.data.message)
+    if (error.response && error.response.data && error.response.data.message) {
+      throw new Error(error.response.data.message)
+    } else {
+      throw new Error(error.message)
+    }
+  }
+}
--- js/packages/web/src/ui/sections/CollectionActivityList/CollectionActivityList.tsx
+++ js/packages/web/src/ui/sections/CollectionActivityList/CollectionActivityList.tsx
@@ -1,76 +1,77 @@
 import React, { FC } from 'react'
 import CN from 'classnames'
-import { ActivityCard } from '@oyster/common'
+import { ActivityCardCollection } from '@oyster/common'
 
 export interface CollectionActivityListProps {
   [x: string]: any
 }
 
-const dummyActivityList = [
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/yytjymgHK5Y64dJLHZlkCB-zbayUpAmLNB49VapL30g',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/ukZ3DgqeaTyx5qifyooXV-rO5CM8CjKnKFJZVlVQFaU',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/LlrUIT49BxcHsSGBXmQcJ70G_jjZMgcPZJh3CxqziuA',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/2HQvY0wYkzabT-3qzFZX-MpECFLFEXX1NhptwjZ98Jw',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/HYGi_EMu8Dhu9cWHaIBdJW0DPJkaZnDr0yJIMy5hlak',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-  {
-    image:
-      'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/-00FUjXIBBOHmnp8v_li6uklY_vfrwR5FvVeVHjdpAM',
-    title: 'Degen Ape #1921',
-    description: 'Sale',
-    price: '49.92',
-    fromAddress: '6gr...Vun5',
-    toAddress: '7hk...JaoW',
-    time: '12 hours ago',
-  },
-]
+// const dummyActivityList = [
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/yytjymgHK5Y64dJLHZlkCB-zbayUpAmLNB49VapL30g',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/ukZ3DgqeaTyx5qifyooXV-rO5CM8CjKnKFJZVlVQFaU',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/LlrUIT49BxcHsSGBXmQcJ70G_jjZMgcPZJh3CxqziuA',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/2HQvY0wYkzabT-3qzFZX-MpECFLFEXX1NhptwjZ98Jw',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/HYGi_EMu8Dhu9cWHaIBdJW0DPJkaZnDr0yJIMy5hlak',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+//   {
+//     image:
+//       'https://cdn-image.solanart.io/unsafe/600x600/filters:format(webp)/arweave.net/-00FUjXIBBOHmnp8v_li6uklY_vfrwR5FvVeVHjdpAM',
+//     title: 'Degen Ape #1921',
+//     description: 'Sale',
+//     price: '49.92',
+//     fromAddress: '6gr...Vun5',
+//     toAddress: '7hk...JaoW',
+//     time: '12 hours ago',
+//   },
+// ]
 
 export const CollectionActivityList: FC<CollectionActivityListProps> = ({
   className,
+  data,
   ...restProps
 }: CollectionActivityListProps) => {
   const CollectionActivityListClasses = CN(
@@ -80,8 +81,8 @@ export const CollectionActivityList: FC<CollectionActivityListProps> = ({
 
   return (
     <div className={CollectionActivityListClasses} {...restProps}>
-      {dummyActivityList.map((activity: any, index: number) => (
-        <ActivityCard key={index} {...activity} />
+      {data.nftActivities.map((activity: any, index: number) => (
+        <ActivityCardCollection key={index} {...activity} />
       ))}
     </div>
   )
