372a2ce0086e2817cc2f3a1d3e8beb54693870e8
nadunlanka <nadun_chat@yahoo.com>
2022-06-10 03:32:54 +0530

    send email via help center

--- js/packages/web/src/api/index.ts
+++ js/packages/web/src/api/index.ts
@@ -5,4 +5,5 @@ export * from './profileApi'
 export * from './offersApi'
 export * from './auctionHouseApi'
 export * from './collectionApi'
+export * from './helpCenter'
 console.log("--node_env--", process.env.NEXT_API_URL)
