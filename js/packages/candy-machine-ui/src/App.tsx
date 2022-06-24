464cdbed6ebbc0466e41e8feb341dfd638621c51
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-06-24 15:48:04 -0500

    linting

--- js/packages/candy-machine-ui/src/App.tsx
+++ js/packages/candy-machine-ui/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import {useMemo} from 'react';
+import { useMemo } from 'react';
 import * as anchor from '@project-serum/anchor';
 import Home from './Home';
-import {DEFAULT_TIMEOUT} from './connection';
-import {clusterApiUrl} from '@solana/web3.js';
-import {WalletAdapterNetwork} from '@solana/wallet-adapter-base';
+import { DEFAULT_TIMEOUT } from './connection';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
   getPhantomWallet,
   getSlopeWallet,
@@ -13,10 +13,13 @@ import {
   getSolletWallet,
 } from '@solana/wallet-adapter-wallets';
 
-import {ConnectionProvider, WalletProvider,} from '@solana/wallet-adapter-react';
-import {WalletDialogProvider} from '@solana/wallet-adapter-material-ui';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
 
-import {createTheme, ThemeProvider} from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core';
 
 const theme = createTheme({
   palette: {
@@ -26,9 +29,7 @@ const theme = createTheme({
 
 const getCandyMachineId = (): anchor.web3.PublicKey | undefined => {
   try {
-    return new anchor.web3.PublicKey(
-      process.env.REACT_APP_CANDY_MACHINE_ID!,
-    );
+    return new anchor.web3.PublicKey(process.env.REACT_APP_CANDY_MACHINE_ID!);
   } catch (e) {
     console.log('Failed to construct CandyMachineId', e);
     return undefined;
@@ -38,17 +39,19 @@ const getCandyMachineId = (): anchor.web3.PublicKey | undefined => {
 let error: string | undefined = undefined;
 
 if (process.env.REACT_APP_SOLANA_NETWORK === undefined) {
-  error = "Your REACT_APP_SOLANA_NETWORK value in the .env file doesn't look right! The options are devnet and mainnet-beta!";
+  error =
+    "Your REACT_APP_SOLANA_NETWORK value in the .env file doesn't look right! The options are devnet and mainnet-beta!";
 } else if (process.env.REACT_APP_SOLANA_RPC_HOST === undefined) {
-  error = "Your REACT_APP_SOLANA_RPC_HOST value in the .env file doesn't look right! Make sure you enter it in as a plain-text url (i.e., https://metaplex.devnet.rpcpool.com/)";
+  error =
+    "Your REACT_APP_SOLANA_RPC_HOST value in the .env file doesn't look right! Make sure you enter it in as a plain-text url (i.e., https://metaplex.devnet.rpcpool.com/)";
 }
 
 const candyMachineId = getCandyMachineId();
-const network = (process.env.REACT_APP_SOLANA_NETWORK ?? 'devnet') as WalletAdapterNetwork;
-const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST ?? anchor.web3.clusterApiUrl('devnet');
-const connection = new anchor.web3.Connection(
-  rpcHost
-);
+const network = (process.env.REACT_APP_SOLANA_NETWORK ??
+  'devnet') as WalletAdapterNetwork;
+const rpcHost =
+  process.env.REACT_APP_SOLANA_RPC_HOST ?? anchor.web3.clusterApiUrl('devnet');
+const connection = new anchor.web3.Connection(rpcHost);
 
 const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
@@ -58,8 +61,8 @@ const App = () => {
       getPhantomWallet(),
       getSolflareWallet(),
       getSlopeWallet(),
-      getSolletWallet({network}),
-      getSolletExtensionWallet({network}),
+      getSolletWallet({ network }),
+      getSolletExtensionWallet({ network }),
     ],
     [],
   );
